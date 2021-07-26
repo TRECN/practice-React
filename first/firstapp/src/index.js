01a9985d2530e8e5f17b67ddbe1337cf7ac412e7
TRECN <rishabh1103@outlook.com>
2021-07-26 22:44:49 +0530

    commit

--- first/firstapp/src/index.js
+++ first/firstapp/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Square extends React.Component {
     render() {
       return (
-        <button className="square">
+        <button className="square" onClick={()=>alert('click')}>
           {this.props.value}
         </button>
       );
@@ -16,5 +16,34 @@ class Square extends React.Component {
     renderSquare(i) {
       return <Square value={i} />;
     }
+    render() {
+        const status = 'Next player: X';
+    
+        return (
+          <div>
+            <div className="status">{status}</div>
+            <div className="board-row">
+              {this.renderSquare(0)}
+              {this.renderSquare(1)}
+              {this.renderSquare(2)}
+            </div>
+            <div className="board-row">
+              {this.renderSquare(3)}
+              {this.renderSquare(4)}
+              {this.renderSquare(5)}
+            </div>
+            <div className="board-row">
+              {this.renderSquare(6)}
+              {this.renderSquare(7)}
+              {this.renderSquare(8)}
+            </div>
+          </div>
+        );
+      }
   }
+  
+  ReactDOM.render(
+    <Board />,
+    document.getElementById('root')
+  );
   
