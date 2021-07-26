e7a9189b48ff824ac698701e612f9373044a4060
TRECN <rishabh1103@outlook.com>
2021-07-26 22:58:03 +0530

    commit

--- first/firstapp/src/index.js
+++ first/firstapp/src/index.js
@@ -29,7 +29,11 @@ class Square extends React.Component {
           };
       }
     renderSquare(i) {
-      return <Square value={i} />;
+      return( 
+      <Square
+       value={this.state.squares[i]}
+       onClick={()=>this.handleClick(i)} />
+      );
     }
     render() {
         const status = 'Next player: X';
