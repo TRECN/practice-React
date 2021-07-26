daeb3e9f2fd28a15a90c781db6b2d8c2ed357db6
TRECN <rishabh1103@outlook.com>
2021-07-26 20:40:36 +0530

    commit

--- first/firstapp/src/index.js
+++ first/firstapp/src/index.js
@@ -1,3 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+class Square extends React.Component {
+    render() {
+      return (
+        <button className="square">
+          {this.props.value}
+        </button>
+      );
+    }
+  }
+  
+  class Board extends React.Component {
+    renderSquare(i) {
+      return <Square value={i} />;
+    }
+  }
+  
