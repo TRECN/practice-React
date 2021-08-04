90f1dcd4f07d90b2913de2a681624cf79a7ef32d
TRECN <rishabh1103@outlook.com>
2021-08-04 19:59:42 +0530

    commit

--- first/Starting/react-todo-app/src/components/TodoContainer.js
+++ first/Starting/react-todo-app/src/components/TodoContainer.js
@@ -8,6 +8,16 @@ class TodoContainer extends React.Component{
                 title:"setup development environment",
                 completed:true
             },
+            {
+                id:2,
+                title:"Develop website and add content",
+                completed:false
+            },
+            {
+                id:3,
+                title:"Deploy to live server",
+                completed:false
+            }
         ]
     };
 
