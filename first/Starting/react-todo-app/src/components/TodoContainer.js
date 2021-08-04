83a8854c6631b915d4407ca6c7ccc958c4fd5e98
TRECN <rishabh1103@outlook.com>
2021-08-04 20:00:06 +0530

    commit

--- first/Starting/react-todo-app/src/components/TodoContainer.js
+++ first/Starting/react-todo-app/src/components/TodoContainer.js
@@ -24,12 +24,6 @@ class TodoContainer extends React.Component{
     render(){
         return(
             
-            <div>
-                <h1>
-                    Hello from create React app
-                </h1>
-                <p>I am in a React Component!</p>
-            </div>
         )
     }
 }
