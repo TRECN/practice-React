929464e6427f62be6296442dc09236b0659f5dc9
TRECN <rishabh1103@outlook.com>
2021-08-05 14:02:52 +0530

    commit

--- first/Starting/react-todo-app/src/components/InputTodo.js
+++ first/Starting/react-todo-app/src/components/InputTodo.js
@@ -14,10 +14,14 @@ class InputTodo extends React.Component{
 
     handleSubmit=e=>{
         e.preventDefault();
-        this.props.addTodoProps(this.state.title);
+        if(this.state.title.trim()){
+            this.props.addTodoProps(this.state.title);
         this.setState({
             title:""
         });
+        }else{
+            alert("Please write Item");
+        }
     };
 
     render(){
