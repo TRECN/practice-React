a26ed3e411285e509e5cc573d103bafc02a86557
TRECN <rishabh1103@outlook.com>
2021-08-05 22:34:38 +0530

    commit

--- first/Starting/react-todo-app/src/components/Header.js
+++ first/Starting/react-todo-app/src/components/Header.js
@@ -1,7 +1,11 @@
 import React from "react";
 const Header = () =>{
+    const headerStyle={
+        padding:"20px 0",
+        lineHeight:"1.5em"
+    }
     return(
-        <header>
+        <header style={headerStyle}>
             <h1
             style={{
                 fontSize:"6rem",
