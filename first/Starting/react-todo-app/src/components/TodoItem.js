5bc9a2ab6dbeab5803bfa031e38d7f6c1335b4f3
TRECN <rishabh1103@outlook.com>
2021-08-05 22:42:48 +0530

    commit

--- first/Starting/react-todo-app/src/components/TodoItem.js
+++ first/Starting/react-todo-app/src/components/TodoItem.js
@@ -1,14 +1,18 @@
 import React from "react"
+import styles from "./TodoItem.module.css"
 function TodoItem(props){
-    return <li>
-        <input type="checkbox" 
-        checked={props.todo.completed}
-        onChange={()=>props.handleChangeProps(props.todo.id)}/>
-        <button onClick={()=>props.deleteTodoProps(props.todo.id)}>
-            delete
+    return (
+        <li className={styles.item}>
+            <input type="checkbox" 
+            className={styles.checkbox}
+            checked={props.todo.completed}
+            onChange={()=>props.handleChangeProps(props.todo.id)}/>
+            <button onClick={()=>props.deleteTodoProps(props.todo.id)}>
+                delete
             </button>
-        {props.todo.title}<br/>
+            {props.todo.title}<br/>
         
         </li>
+    )
 }
 export default TodoItem
