a316906198d5f427b155c74dfcefffee0118fd5f
TRECN <rishabh1103@outlook.com>
2021-08-06 14:10:04 +0530

    commit

--- first/Starting/react-todo-app/src/components/TodoItem.js
+++ first/Starting/react-todo-app/src/components/TodoItem.js
@@ -1,11 +1,13 @@
 import React from "react"
 import styles from "./TodoItem.module.css"
 
-handleEditing = () =>{
-    console.log("edit mode activated");
-}
 
-function TodoItem(props){
+
+class TodoItem extends React.Component{
+    handleEditing = () =>{
+        console.log("edit mode activated");
+    }
+    render(){
     const completedStyle={
         fontStyle:"italic",
         color:"#595959",
@@ -13,16 +15,17 @@ function TodoItem(props){
         textDecoration:"line-through",
 
     }
-    const {completed, id, title} = props.todo;
+    const {completed, id, title} = this.props.todo;
+    
     return (
         <li className={styles.item}>
             <div onDoubleClick={this.handleEditing}>
                 <input type="checkbox" 
                 className={styles.checkbox}
                 checked={completed}
-                onChange={()=>props.handleChangeProps(id)}
+                onChange={()=>this.props.handleChangeProps(id)}
                 />
-                <button onClick={()=>props.deleteTodoProps(id)}>
+                <button onClick={()=>this.props.deleteTodoProps(id)}>
                     delete
                 </button>
                 <span style={completed ? completedStyle:null}>
@@ -30,6 +33,6 @@ function TodoItem(props){
                 </span>
             </div>
         </li>
-    )
+    )}
 }
 export default TodoItem
