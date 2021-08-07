8948dd686f0ecb7d43a0179b0c0d25ecc5259b3b
TRECN <rishabh1103@outlook.com>
2021-08-07 22:27:25 +0530

    commit

--- first/Starting/react-todo-app/src/functionBased/components/InputTodo.js
+++ first/Starting/react-todo-app/src/functionBased/components/InputTodo.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import {FaPlusCircle} from "react-icons/fa"
 
 const InputTodo = props =>{
     const [inputText, setInputText]=useState({
@@ -35,7 +36,7 @@ const InputTodo = props =>{
                 onChange={onChange}/>
 
                 <button className="input-submit">
-                    Submit
+                    <FaPlusCircle/>
                 </button>
         </form>
 
