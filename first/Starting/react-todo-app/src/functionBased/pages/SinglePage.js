907aa3e7f657ae6cd12fb550f90857c85d055e00
TRECN <rishabh1103@outlook.com>
2021-08-09 22:31:23 +0530

    commit

--- first/Starting/react-todo-app/src/functionBased/pages/SinglePage.js
+++ first/Starting/react-todo-app/src/functionBased/pages/SinglePage.js
@@ -15,9 +15,19 @@ const SinglePage=()=>{
             title: "About the Author",
             description:
               "This app was developed by Ibas Majid, a self-taught web developer and a technical writer. He is opened to freelance Gig. So go ahead and connect with ibas on Twitter @ibaslogic.",
-          },
+        },
     ]
-    return <div>Hello from single page</div>
+
+    const {slug}=useParams()
+    const aboutContent =aboutData.find(item=>item.slug===slug)
+    const {title,discription}=aboutContent
+
+    return (
+        <div>
+            <h1>{title}</h1>
+            <p>{discription}</p>
+        </div>
+    )
 
 }
 
