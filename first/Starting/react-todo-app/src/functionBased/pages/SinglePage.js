4a6bdf78a9942f958d7dc1117274acae46ea3678
TRECN <rishabh1103@outlook.com>
2021-08-09 22:27:28 +0530

    commit

--- first/Starting/react-todo-app/src/functionBased/pages/SinglePage.js
+++ first/Starting/react-todo-app/src/functionBased/pages/SinglePage.js
@@ -3,6 +3,15 @@ import { useParams } from "react-router";
 
 const SinglePage=()=>{
     console.log(useParams())
+    const aboutData=[
+        {
+            slug:"about-app",
+            title:"About the App",
+            discription:
+            "In this app, you can add, delete, submit and edit items. To edit items, simply double click on it. Once you are done, press the enter key to resubmit. This app will persist your data in the browser local storage. So whether you reload, close your app or reopened it, you still have access to your to-dos items.",
+        },
+        
+    ]
     return <div>Hello from single page</div>
 
 }
