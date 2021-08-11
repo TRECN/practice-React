844a26038b878d98af9a9bef8c64155a3f4f62e3
TRECN <rishabh1103@outlook.com>
2021-08-11 16:42:25 +0530

    commit

--- first/restaurant-web/src/index.js
+++ first/restaurant-web/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './Components/Card';
 
+//importing css
+import "./Components/index.css"
 
 ReactDOM.render(
   <React.StrictMode>
