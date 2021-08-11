5ebb2618331467911a4d7ce9065dab53f241d7f9
TRECN <rishabh1103@outlook.com>
2021-08-11 17:03:16 +0530

    commit

--- first/restaurant-web/src/index.js
+++ first/restaurant-web/src/index.js
@@ -4,9 +4,11 @@ import Card from './Components/Card';
 
 //importing css
 import "./Components/index.css"
+import Navbar from './Components/Navbar';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Navbar/>
     <Card/>
   </React.StrictMode>,
   document.getElementById('root')
