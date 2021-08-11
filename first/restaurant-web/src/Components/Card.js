92ab1d87cac817abaabf7b5374d4467b798c16a0
TRECN <rishabh1103@outlook.com>
2021-08-11 17:03:20 +0530

    commit

--- first/restaurant-web/src/Components/Card.js
+++ first/restaurant-web/src/Components/Card.js
@@ -8,7 +8,7 @@ const Card = () =>{
             <div className="card">
                 <img src={egg} style={{width:"85%"}}/>
                 <h1>EGG and Bread</h1>
-                <p className="price">Rs. 50.00/</p>
+                <p className="price">Rs. 50.00/-</p>
                 <p><button>Add to cart</button></p>
             </div>
         
