f769cdcfdcd2c581e6997d91e94ab8f993e76f18
TRECN <rishabh1103@outlook.com>
2021-08-12 21:51:37 +0530

    commit

--- first/exampleproject/src/App.js
+++ first/exampleproject/src/App.js
@@ -1,10 +1,10 @@
 
 import './App.css';
-
+import Navbar from './Components/Navbar'
 function App() {
   return (
     <div className="App">
-      <h1>Rishabh</h1>
+        <Navbar/>
     </div>
   );
 }
