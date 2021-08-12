a803ecbe519cf63cc855efb697b2276f92da59cf
TRECN <rishabh1103@outlook.com>
2021-08-12 22:17:34 +0530

    commit

--- first/exampleproject/src/Components/Button.js
+++ first/exampleproject/src/Components/Button.js
@@ -7,8 +7,11 @@ const SIZE=['btn--medium', 'btn-large', 'btn-mobile', 'btn--wide']
 
 const COLOR =['primary','blue','red','green']
 
-export const Button =({children,type,onClick,buttonStyle,ButonSize,buttonColor})=>{
+export const Button =({children,type,onClick,buttonStyle,buttonSize,buttonColor})=>{
     const checkButtonStyle=STYLES.includes(buttonStyle)?buttonStyle:STYLES[0]
+    const checkButtonSize=SIZE.includes(buttonSize)?buttonSize:SIZE[0]
+    const checkButtonColor=COLOR.includes(buttonColor)?buttonColor:COLOR[0]
+
     return(
         <button className={`btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonColor}`} onClick={onClick} type={type}>{children}</button>
     )
