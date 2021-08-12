b5686cea5b5abdb03e85715241b96dcf3c4f5692
TRECN <rishabh1103@outlook.com>
2021-08-12 21:47:38 +0530

    commit

--- first/exampleproject/src/Components/Navbar.js
+++ first/exampleproject/src/Components/Navbar.js
@@ -3,7 +3,16 @@ import React from 'react'
 function Navbar() {
     return (
         <>
-            <h1></h1>
+            <div className="navbar">
+                <div className="navbar-container container">
+                    <Link to='/' className="navbar-logo">
+                        LEBAR
+                    </Link>
+                    <div className="menu-icon">
+                        
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
