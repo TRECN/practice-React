de4a3ec84ecfefd06dda65a3e4a7c359a20fa80d
TRECN <rishabh1103@outlook.com>
2021-08-14 21:09:28 +0530

    commit

--- first/exampleproject/src/Components/Button.js
+++ first/exampleproject/src/Components/Button.js
@@ -9,8 +9,8 @@ const COLOR =['primary','blue','red','green']
 
 const Button =({children,type,onClick,buttonStyle,buttonSize,buttonColor})=>{
     const checkButtonStyle=STYLES.includes(buttonStyle)?buttonStyle:STYLES[0]
-    const checkButtonSize=SIZE.includes(buttonSize)?buttonSize:SIZE[0]
-    const checkButtonColor=COLOR.includes(buttonColor)?buttonColor:COLOR[0]
+    const checkButtonSize=STYLES.includes(buttonSize)?buttonSize:SIZE[0]
+    const checkButtonColor=STYLES.includes(buttonColor)?buttonColor:null
 
     return(
         <button className={`btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonColor}`} onClick={onClick} type={type}>{children}</button>
