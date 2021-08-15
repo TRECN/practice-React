f89c5f978dadca274279d1bbeb3ef60e5135d65d
TRECN <rishabh1103@outlook.com>
2021-08-15 21:31:14 +0530

    commit

---
+++ first/exampleproject/src/Components/Pages/HomePage/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroSection from '../../HeroSection'
+import {homeObjOne} from './Data'
+ftion Home() {
+    return (
+        <>
+          <HeroSection {...homeObjOne} />
+        </>
+    )
+}
+
+export default Home
