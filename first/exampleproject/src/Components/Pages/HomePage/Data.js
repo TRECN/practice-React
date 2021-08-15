d41281ea45150a908aab906f5ac2e47800d0820a
TRECN <rishabh1103@outlook.com>
2021-08-15 21:36:19 +0530

    commit

---
+++ first/exampleproject/src/Components/Pages/HomePage/Data.js
@@ -0,0 +1,13 @@
+export const homeObjone={
+    lightBg:false,
+    lightText:true,
+    lightTextDesc:true,
+    topLine:'Exclusive Access',
+    headline:'Unlimited Transaction with zero fees',
+    description:'Get access to out exclusive diamond card that allows you to send unlimited transaction without getting charged any fee',
+    buttonLable:'Get Started',
+    imgStaring:'',
+    img:'images/svg-1.svg'
+    alt:'Credit Card'
+
+}
