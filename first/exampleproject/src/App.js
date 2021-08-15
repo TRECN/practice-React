48d3b732441eb91437eafead7c1255c196bf5513
TRECN <rishabh1103@outlook.com>
2021-08-15 21:21:14 +0530

    commit

--- first/exampleproject/src/App.js
+++ first/exampleproject/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
+import HeroSection from './Components/HeroSection';
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar/>
+        <HeroSection/>
       </Router>
     </div>
   );
