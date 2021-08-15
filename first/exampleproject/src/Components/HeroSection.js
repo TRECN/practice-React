08a878e02df4f3c44c8c60848a5f599da7ddf80e
TRECN <rishabh1103@outlook.com>
2021-08-15 21:12:06 +0530

    commit

---
+++ first/exampleproject/src/Components/HeroSection.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import{Button} from './Button'
+import {Link} from 'react-router-dom'
+import './HeroSection.css'
+function HeroSection() {
+    return (
+        <>
+           
+        </>
+    )
+}
+
+export default HeroSection
