e984198ae95353476ffbd57563f3d8905f1a84b5
TRECN <rishabh1103@outlook.com>
2021-08-15 21:21:20 +0530

    commit

--- first/exampleproject/src/Components/HeroSection.js
+++ first/exampleproject/src/Components/HeroSection.js
@@ -2,10 +2,26 @@ import React from 'react'
 import{Button} from './Button'
 import {Link} from 'react-router-dom'
 import './HeroSection.css'
-function HeroSection() {
+function HeroSection({
+    lightBg, topLine,lightText, lightTextDesc, headline, discription,
+    buttonLable, img,alt,imgStart
+}) {
+    
     return (
         <>
-           
+           <div className={lightBg ? 'home__hero-section':'home__hero-section darkBg'}>
+               <div className="container">
+                   <div className="row home__hero-row" 
+                   style={{display:'flex', flexDirection:imgStart==='start'?'row-reverse':'row'}}>
+                       <div className="col">
+                           <div className="home__hero-text-wrapper">
+                               <div className="top-line">{topLine}</div>
+                               <h1 className={}>{headline}</h1>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+           </div>
         </>
     )
 }
