e0c2adae16045cc1a25fc3ec44df53697c7c5447
TRECN <rishabh1103@outlook.com>
2021-08-16 22:02:05 +0530

    commit

--- first/exampleproject/src/Components/Pages/HomePage/Data.js
+++ first/exampleproject/src/Components/Pages/HomePage/Data.js
@@ -12,4 +12,18 @@ const homeObjOne={
 
 }
 
+const homeObjTwo={
+    lightBg:false,
+    lightText:true,
+    lightTextDesc:true,
+    topLine:'100% SECURE',
+    headline:'Staay protected 24/7 anywhere anytime',
+    description:
+        'We have you covered no matter where you are located. Over 140 locations worldwide to ensure you have access anytime ',
+    buttomeLabel:'Learn More',
+    imgStart:'',
+    img:'images/svg-5.svg',
+    alt:'Vault'
+};
+
 export default homeObjOne
