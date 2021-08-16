4bd638839e8bccf32a021a5c0bbb1339d39d297b
TRECN <rishabh1103@outlook.com>
2021-08-16 23:01:49 +0530

    commit

--- first/exampleproject/src/Components/Pages/Footer/Footer.js
+++ first/exampleproject/src/Components/Pages/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Footer.css'
-import {Button} from '../..Button'
+import Button from '../../Button'
 import {Link} from 'react-router-dom'
 import {
     FaFaceBook,
@@ -12,8 +12,12 @@ import {
 import {MdFingerprint} from 'react-icons/md'
 function Footer() {
     return (
-        <div>
-            
+        <div className='footer-container'>
+            <section className="footer-subscription">
+                <p className="footer-subscription-heading">
+                    Join our exclusive membership to recive the latest news and trends
+                </p>
+            </section>
         </div>
     )
 }
