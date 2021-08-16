d2d1bce8bbc60eac530d6bfb85d91fa424985f64
TRECN <rishabh1103@outlook.com>
2021-08-16 23:05:30 +0530

    commit

--- first/exampleproject/src/Components/Pages/Footer/Footer.js
+++ first/exampleproject/src/Components/Pages/Footer/Footer.js
@@ -17,6 +17,19 @@ function Footer() {
                 <p className="footer-subscription-heading">
                     Join our exclusive membership to recive the latest news and trends
                 </p>
+                <p className="footer-subscription-text">
+                    You can unsubscribe at any time.
+                </p>
+                <div className="input-areas">
+                    <form>
+                        <input 
+                        className="footer-input" 
+                        name='email' 
+                        type='email'
+                        placeholder='Your Email'/>
+                    </form>
+                    <Button buttonStyle='btn--outline'>Subscribe</Button>
+                </div>
             </section>
         </div>
     )
