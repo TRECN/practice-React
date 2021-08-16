0f6a7a85827408be396299fd61cef3e59ce0a50f
TRECN <rishabh1103@outlook.com>
2021-08-16 22:16:19 +0530

    commit

--- first/exampleproject/src/Components/Pages/HomePage/Home.js
+++ first/exampleproject/src/Components/Pages/HomePage/Home.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import HeroSection from '../../HeroSection'
-import {homeObjOne} from './Data'
-import {homeObjTwo} from './Data'
-import { homeObjThree } from './Data'
-import { homeObjFour } from './Data'
+import {homeObjOne,homeObjTwo,homeObjThree,homeObjFour } from './Data'
+
 function Home() {
     return (
         <>
