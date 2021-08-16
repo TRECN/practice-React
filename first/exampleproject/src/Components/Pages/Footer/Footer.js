b5a4b760cd4891e0c059124d64856e46e1ba0b29
TRECN <rishabh1103@outlook.com>
2021-08-16 22:56:12 +0530

    commit

---
+++ first/exampleproject/src/Components/Pages/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Footer.css'
+import {Button} from '../..Button'
+import {Link} from 'react-router-dom'
+import {
+    FaFaceBook,
+    FaInsgtagram,
+    FaToutube,
+    FaTwitter,
+    FaLinkedin
+}from 'react-icons/fa'
+import {MdFingerprint} from 'react-icons/md'
+function Footer() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Footer
