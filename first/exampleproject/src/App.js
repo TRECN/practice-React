e460259a70420a004f2a636a442fe036570de6f0
TRECN <rishabh1103@outlook.com>
2021-08-17 21:42:47 +0530

    commit

--- first/exampleproject/src/App.js
+++ first/exampleproject/src/App.js
@@ -5,6 +5,7 @@ import { Route } from 'react-router';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import Home from './Components/Pages/HomePage/Home';
 import Footer from './Components/Pages/Footer/Footer';
+import Pricing from './Components/Pricing';
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route path='/' exact component={Home}></Route>
         </Switch>
+        <Pricing/>
         <Footer/>
       </Router>
     </div>
