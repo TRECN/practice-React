8d9a39890f1f76315bea8e9627cc1aaa49f5b432
TRECN <rishabh1103@outlook.com>
2021-08-17 22:25:34 +0530

    commit

--- first/exampleproject/src/Components/Pricing.js
+++ first/exampleproject/src/Components/Pricing.js
@@ -47,7 +47,7 @@ function Pricing() {
                                         <li>3.5% Cash Back</li>
                                         <li>$100,000 Limit</li>
                                     </ul>
-                                    <Button buttonSize='btn--wide' buttonColor='primary'>
+                                    <Button buttonSize='btn--wide' buttonColor='blue'>
                                         Choose Plan
                                     </Button>
                                 </div>
