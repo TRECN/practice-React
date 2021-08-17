f3bb3e964a0751bd5caa518f276c9428625c83f7
TRECN <rishabh1103@outlook.com>
2021-08-17 23:44:04 +0530

    commit

--- first/exampleproject/src/Components/Pages/HomePage/Data.js
+++ first/exampleproject/src/Components/Pages/HomePage/Data.js
@@ -1,55 +1,55 @@
 export const homeObjOne={
-    lightBg:false,
-    lightText:true,
-    lightTextDesc:true,
-    topLine:'Exclusive Access',
-    headline:'Unlimited Transaction with zero fees',
-    description:'Get access to out exclusive diamond card that allows you to send unlimited transaction without getting charged any fee',
-    buttonLable:'Get Started',
-    imgStart:'',
-    img:'img/svg-1.svg',
-    alt:'Credit Card'
+  lightBg:false,
+  lightText:true,
+  lightTextDesc:true,
+  topLine:'Exclusive Access',
+  headline:'Unlimited Transaction with zero fees',
+  description:'Get access to out exclusive diamond card that allows you to send unlimited transaction without getting charged any fee',
+  buttonLable:'Get Started',
+  imgStart:'',
+  img:'images/svg-1.svg',
+  alt:'Credit Card'
 
 }
 
 export const homeObjTwo={
-    lightBg:true,
-    lightText:false,
-    lightTextDesc:false,
-    topLine:'100% SECURE',
-    headline:'Stay protected 24/7 anywhere anytime',
-    description:
-        'We have you covered no matter where you are located. Over 140 locations worldwide to ensure you have access anytime ',
-    buttonLable:'Learn More',
-    imgStart:'start',
-    img:'img/svg-5.svg',
-    alt:'Vault'
+  lightBg:true,
+  lightText:false,
+  lightTextDesc:false,
+  topLine:'100% SECURE',
+  headline:'Stay protected 24/7 anywhere anytime',
+  description:
+      'We have you covered no matter where you are located. Over 140 locations worldwide to ensure you have access anytime ',
+  buttonLable:'Learn More',
+  imgStart:'start',
+  img:'images/svg-5.svg',
+  alt:'Vault'
 };
 export const homeObjThree = {
-    lightBg:false,
-    lightText:true,
-    lightTextDesc:true,
-    topLine: 'EASY SETUP',
-    headline: 'Super fast and simple onboarding process',
-    description:
-      "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
-    buttonLable: 'Start Now',
-    imgStart: '',
-    img: 'img/svg-7.svg',
-    alt: 'Vault'
-  };
-  
- 
-  export const homeObjFour = {
-    lightBg:true,
-    lightText:false,
-    lightTextDesc:false,
-    topLine: 'DATA ANALYTICS',
-    headline: 'Every transaction is stored on our secure cloud database',
-    description:
-      'Never ever have to worry again about saved reciepts. We store your data, so you can access it anytime.',
-    buttonLable: 'Sign Up Now',
-    imgStart: 'start',
-    img: 'img/svg-8.svg',
-    alt: 'Vault'
-  };
+  lightBg:false,
+  lightText:true,
+  lightTextDesc:true,
+  topLine: 'EASY SETUP',
+  headline: 'Super fast and simple onboarding process',
+  description:
+    "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+  buttonLable: 'Start Now',
+  imgStart: '',
+  img: 'images/svg-7.svg',
+  alt: 'Vault'
+};
+
+
+export const homeObjFour = {
+  lightBg:true,
+  lightText:false,
+  lightTextDesc:false,
+  topLine: 'DATA ANALYTICS',
+  headline: 'Every transaction is stored on our secure cloud database',
+  description:
+    'Never ever have to worry again about saved reciepts. We store your data, so you can access it anytime.',
+  buttonLable: 'Sign Up Now',
+  imgStart: 'start',
+  img: 'images/svg-8.svg',
+  alt: 'Vault'
+};
