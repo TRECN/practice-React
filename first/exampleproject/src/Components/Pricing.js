ad36362a7ffaca2f9ac8eecde1965e576357d1eb
TRECN <rishabh1103@outlook.com>
2021-08-17 21:40:05 +0530

    commit

--- first/exampleproject/src/Components/Pricing.js
+++ first/exampleproject/src/Components/Pricing.js
@@ -16,7 +16,25 @@ function Pricing() {
                                     <div className="icon">
                                         <FaFire/>
                                     </div>
-                                    <h3>Stater</h3>
+                                    <h3>Starter</h3>
+                                    <h4>$58.99</h4>
+                                    <p>per month</p>
+                                    <ul className="pricing__container-features">
+                                        <li>100 Transaction</li>
+                                        <li>2% Cash Back</li>
+                                        <li>$10,000 Limit</li>
+                                    </ul>
+                                    <Button buttonSize='btn--wide' buttonColor='primary'>
+                                        Choose Plan
+                                    </Button>
+                                </div>
+                            </Link>
+                            <Link to="/sign-up" className='pricing__container-card'>
+                                <div className="pricing__container-cardInfo">
+                                    <div className="icon">
+                                        <BsXDiamondFill/>
+                                    </div>
+                                    <h3>Starter</h3>
                                     <h4>$58.99</h4>
                                     <p>per month</p>
                                     <ul className="pricing__container-features">
