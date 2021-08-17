90f72a83576510c34938d8d2a8569ca3e5080485
TRECN <rishabh1103@outlook.com>
2021-08-17 21:31:20 +0530

    commit

---
+++ first/exampleproject/src/Components/Pricing.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Pricing() {
+    return (
+        <div>
+            <div className="pricing__section">
+                <div className="pricing__wrapper">
+                    <div className="pricing__hrading">
+                        <div className="pricing__container">
+                            <Link to="/sign-up">
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Pricing
