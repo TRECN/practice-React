aafdcdfdefb606c5d2286e4d93845a60a771bbd3
TRECN <rishabh1103@outlook.com>
2021-08-18 15:53:22 +0530

    commit

--- first/exampleproject/src/Components/Pages/Data.js
+++ first/exampleproject/src/Components/Pages/Data.js
@@ -44,7 +44,7 @@ export const homeObjFour = {
   lightBg:true,
   lightText:false,
   lightTextDesc:false,
-  topLine: 'DATA ANALYTICS',
+  topLine: 'ANALYTICS',
   headline: 'Every transaction is stored on our secure cloud database',
   description:
     'Never ever have to worry again about saved reciepts. We store your data, so you can access it anytime.',
