a58ac55b62704623bf35ab3d1be08424663bad18
TRECN <rishabh1103@outlook.com>
2021-08-18 16:22:48 +0530

    commit

--- first/exampleproject/src/Components/Pages/Data.js
+++ first/exampleproject/src/Components/Pages/Data.js
@@ -8,7 +8,7 @@ export const homeObjOne={
   description:'Get access to out exclusive diamond card that allows you to send unlimited transaction without getting charged any fee',
   buttonLable:'Get Started',
   imgStart:'',
-  img:'assets/svg-1.png',
+  img:'/assets/svg-1.png',
   alt:'Credit Card'
 
 }
@@ -23,7 +23,7 @@ export const homeObjTwo={
       'We have you covered no matter where you are located. Over 140 locations worldwide to ensure you have access anytime ',
   buttonLable:'Learn More',
   imgStart:'start',
-  img:'assets/svg-5.png',
+  img:'/assets/svg-5.png',
   alt:'Vault'
 };
 export const homeObjThree = {
@@ -36,7 +36,7 @@ export const homeObjThree = {
     "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
   buttonLable: 'Start Now',
   imgStart: '',
-  img: 'assets/svg-7.png',
+  img: '/assets/svg-7.png',
   alt: 'Vault'
 };
 
@@ -45,12 +45,12 @@ export const homeObjFour = {
   lightBg:true,
   lightText:false,
   lightTextDesc:false,
-  topLine: 'ANALYTICS',
+  topLine: 'DATA ANALYTICS',
   headline: 'Every transaction is stored on our secure cloud database',
   description:
     'Never ever have to worry again about saved reciepts. We store your data, so you can access it anytime.',
   buttonLable: 'Sign Up Now',
   imgStart: 'start',
-  img: 'assets/svg-8.png',
+  img: '/assets/svg-8.png',
   alt: 'Vault'
 };
