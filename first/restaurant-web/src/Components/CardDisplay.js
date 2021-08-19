075cca7d98fddf2508f7f9638bb5c16ec1590349
TRECN <rishabh1103@outlook.com>
2021-08-19 22:17:46 +0530

    commit

---
+++ first/restaurant-web/src/Components/CardDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './Card'
+
+function CardDisplay() {
+    return (
+        <div>
+            <Card/>
+        </div>
+    )
+}
+
+export default CardDisplay
