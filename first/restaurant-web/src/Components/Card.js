a3f63da145794b34cae0efd9d6f8792c1f6fa153
TRECN <rishabh1103@outlook.com>
2021-08-19 22:38:19 +0530

    commit

--- first/restaurant-web/src/Components/Card.js
+++ first/restaurant-web/src/Components/Card.js
@@ -9,11 +9,26 @@ const Card = (
         img
     })=>{
 
+        // const setImg=()=>{
+        //     if(img==='1')
+        //         return svg
+        //     else
+        //         if(img==='2')
+        //             return svg1
+        //     else
+        //     if(img==='3')
+        //         return svg2
+        //     else
+        //     if(img==='4')
+        //         return svg3
+        // }
     return (
         <> 
             <div className="container">
                 <div className="card">
-                    <img src={egg} style={{width:"85%"}}/>
+                    <div className="imgContainer">
+                        <img src={egg} style={{width:"85%"}}/>
+                    </div>
                     <h1>{product}</h1>
                     <p className="price">{price}</p>
                     <p><button>{btnName}</button></p>
