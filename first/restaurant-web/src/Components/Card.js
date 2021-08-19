ea25f198a039a1b4b9281993a510120e78f092a8
TRECN <rishabh1103@outlook.com>
2021-08-19 21:32:59 +0530

    commit

--- first/restaurant-web/src/Components/Card.js
+++ first/restaurant-web/src/Components/Card.js
@@ -1,5 +1,6 @@
 import React from "react";
 import egg from "../assets/egg.jpeg"
+import image from '../assets/images.jpeg'
 
 const Card = () =>{
 
@@ -10,6 +11,12 @@ const Card = () =>{
                     <img src={egg} style={{width:"85%"}}/>
                     <h1>EGG and Bread</h1>
                     <p className="price">Rs. 50.00/-</p>
+                    <p><button>Order</button></p>
+                </div>
+                <div className="card">
+                    <img src={image} style={{width:"80%"}}/>
+                    <h1>PanCake</h1>
+                    <p className="price">Rs. 100.00/-</p>
                     <p><button>Order</button></p>
                 </div>
             </div>
