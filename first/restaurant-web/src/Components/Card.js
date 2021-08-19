225f8bee9befc6c24feef2667408414e0e75b77e
TRECN <rishabh1103@outlook.com>
2021-08-19 21:18:45 +0530

    commit

--- first/restaurant-web/src/Components/Card.js
+++ first/restaurant-web/src/Components/Card.js
@@ -4,14 +4,14 @@ import egg from "../assets/egg.jpeg"
 const Card = () =>{
 
     return (
-        <>
-            <div className="card">
-                <img src={egg} style={{width:"85%"}}/>
-                <h1>EGG and Bread</h1>
-                <p className="price">Rs. 50.00/-</p>
-                <p><button>Order</button></p>
+        <>  <div className="container">
+                <div className="card">
+                    <img src={egg} style={{width:"85%"}}/>
+                    <h1>EGG and Bread</h1>
+                    <p className="price">Rs. 50.00/-</p>
+                    <p><button>Order</button></p>
+                </div>
             </div>
-        
         </>
     )
 
