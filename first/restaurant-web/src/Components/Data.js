1498a655a5aeddca38a0322f30186a35ec6cc2ae
TRECN <rishabh1103@outlook.com>
2021-08-20 21:12:41 +0530

    commit

--- first/restaurant-web/src/Components/Data.js
+++ first/restaurant-web/src/Components/Data.js
@@ -4,3 +4,33 @@ export const cardObjone={
     btnName:'Order',
     img:'1'
 }
+export const cardObjone={
+    product:'EGG and Bread',
+    price:'rs. 50.00/-',
+    btnName:'Order',
+    img:'2'
+}
+export const cardObjone={
+    product:'EGG and Bread',
+    price:'rs. 50.00/-',
+    btnName:'Order',
+    img:'3'
+}
+export const cardObjone={
+    product:'EGG and Bread',
+    price:'rs. 50.00/-',
+    btnName:'Order',
+    img:'4'
+}
+export const cardObjone={
+    product:'EGG and Bread',
+    price:'rs. 50.00/-',
+    btnName:'Order',
+    img:'5'
+}
+export const cardObjone={
+    product:'EGG and Bread',
+    price:'rs. 50.00/-',
+    btnName:'Order',
+    img:'1'
+}
