5e1b4e793f1ca710c87254dbcbbbb34b1e6d62e5
TRECN <rishabh1103@outlook.com>
2021-08-25 21:21:00 +0530

    commit

--- first/zomato/src/Components/Navbar/Navbar.js
+++ first/zomato/src/Components/Navbar/Navbar.js
@@ -44,10 +44,27 @@ function Navbar() {
                 <a className="bar-icon" onClick={myFunction}>
                     <FaBars/>
                 </a>
+                
                 <div className="img-container">
                     <a href="/">
                         <img src={logo} alt="Zomato" style={{width:'130px'}}/>
                     </a>
+                </div>
+            </div>
+            <div className="container3">
+                <ul className='links'>
+                    <li><a href="/">Log in</a></li>
+                    <li><a href="/">Sign up</a></li>
+                </ul>
+                <div className="search-bar">
+                    <div className="drop">
+                        <TiLocation className='location-icon'/>
+                        <select className='dropdown'>
+                            <option>rishabh</option>
+                        </select>
+                    </div>
+                    <BsSearch className='search-icon'/>
+                    <input type="text" placeholder='Search for restaurant, cuisine or dish' name="" className='search-text' />
                 </div>
             </div>
         </>
