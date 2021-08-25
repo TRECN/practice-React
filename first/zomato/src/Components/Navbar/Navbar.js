da7b86e42426767e329f135f9e35800363e1638a
TRECN <rishabh1103@outlook.com>
2021-08-25 21:48:31 +0530

    commit

--- first/zomato/src/Components/Navbar/Navbar.js
+++ first/zomato/src/Components/Navbar/Navbar.js
@@ -57,15 +57,17 @@ function Navbar() {
                     <li><a href="/">Log in</a></li>
                     <li><a href="/">Sign up</a></li>
                 </ul>
-                <div className="search-bar">
-                    <div className="drop">
-                        <TiLocation className='location-icon'/>
-                        <select className='dropdown'>
-                            <option>rishabh</option>
-                        </select>
+                <div>
+                    <div className="search-bar">
+                        <div className="drop">
+                            <TiLocation className='location-icon'/>
+                            <select className='dropdown'>
+                                <option>rishabh</option>
+                            </select>
+                        </div>
+                        <BsSearch className='search-icon'/>
+                        <input type="text" placeholder='Search for restaurant, cuisine or dish' name="" className='search-text' />
                     </div>
-                    <BsSearch className='search-icon'/>
-                    <input type="text" placeholder='Search for restaurant, cuisine or dish' name="" className='search-text' />
                 </div>
             </div>
         </>
