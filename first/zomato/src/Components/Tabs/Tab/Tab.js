1be20eb696298d7312ca30fc7bc92c9b96a3f3a6
TRECN <rishabh1103@outlook.com>
2021-08-26 21:48:59 +0530

    commit

--- first/zomato/src/Components/Tabs/Tab/Tab.js
+++ first/zomato/src/Components/Tabs/Tab/Tab.js
@@ -3,7 +3,7 @@ import delivery_active from '../../../assets/tabs/delivery_active.webp'
 import './Tab.css'
 function Tab() {
     return (
-        <div>
+        <div className='tab'>
             <div className="logo">
                 <img src={delivery_active} alt="" />
             </div>
