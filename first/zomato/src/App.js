6db9dc8122ff66b0205432bd2e3f4c796298af3b
TRECN <rishabh1103@outlook.com>
2021-08-26 21:34:00 +0530

    commit

--- first/zomato/src/App.js
+++ first/zomato/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Tabs from './Components/Tabs/Tabs'
 
 function App() {
     return (
         <>
             <Navbar/>
+            <Tabs/>            
         </>
     )
 }
