277dbdce0ebf6c4b900fef3bb549ae2261cbfb92
TRECN <rishabh1103@outlook.com>
2021-08-28 23:29:29 +0530

    commit

--- first/zomato/src/Components/Tabs/Tabs.js
+++ first/zomato/src/Components/Tabs/Tabs.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Tab from './Tab/Tab'
 import './Tabs.css'
+import {TabObjone,TabObjtwo,TabObjthree,TabObjfour} from '../Tabs/Tab/Data'
 function Tabs() {
     return (
         <div className='tabs'>
