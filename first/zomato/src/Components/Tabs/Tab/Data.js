10928b2f0217619a0a21b470501f3ebd9f754e8f
TRECN <rishabh1103@outlook.com>
2021-08-28 23:16:14 +0530

    commit

--- first/zomato/src/Components/Tabs/Tab/Data.js
+++ first/zomato/src/Components/Tabs/Tab/Data.js
@@ -2,3 +2,7 @@ export const TabObjone={
     img:'../../../assets/tabs/delivery_active.webp',
     head: 'Delivery'
 }
+export const TabObjone={
+    img:'../../../assets/tabs/delivery_active.webp',
+    head: 'Delivery'
+}
