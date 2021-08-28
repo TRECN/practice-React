8ae59aea72da4fd3afb92e04baa9f81d97917335
TRECN <rishabh1103@outlook.com>
2021-08-28 23:17:45 +0530

    commit

--- first/zomato/src/Components/Tabs/Tab/Data.js
+++ first/zomato/src/Components/Tabs/Tab/Data.js
@@ -1,7 +1,7 @@
 import delivery_active from '../../../assets/tabs/delivery_active.webp'
 
 export const TabObjone={
-    img:{delivery_active},
+    img:delivery_active,
     head: 'Delivery'
 }
 export const TabObjone={
