44db36c14465533a0b150c459f42ba1883e65a31
TRECN <rishabh1103@outlook.com>
2021-08-28 23:23:31 +0530

    commit

--- first/zomato/src/Components/Tabs/Tab/Data.js
+++ first/zomato/src/Components/Tabs/Tab/Data.js
@@ -1,7 +1,7 @@
 import delivery_active from '../../../assets/tabs/delivery_active.webp'
 import plate_active from '../../../assets/tabs/plate_active.webp'
 import nightlife_active from '../../../assets/tabs/Nightlife_active.webp'
-import  from '../../../assets/tabs/.webp'
+import nutrition_active from '../../../assets/tabs/Nutrition_active.webp'
 
 export const TabObjone={
     img:delivery_active,
