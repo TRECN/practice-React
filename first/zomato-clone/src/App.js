c20100a69e6a20bede72617c2cf2816a80c37877
TRECN <rishabh1103@outlook.com>
2021-08-29 22:36:38 +0530

    commit

--- first/zomato-clone/src/App.js
+++ first/zomato-clone/src/App.js
@@ -9,7 +9,7 @@ function App() {
             <BrowserRouter>
                 <Navbar/>
                 <Tabs/>
-                </BrowserRouter>
+            </BrowserRouter>
 
         </div>
     )
