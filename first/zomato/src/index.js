99d3b193f1261671d01c45839e6edf36396c6b50
TRECN <rishabh1103@outlook.com>
2021-08-29 22:10:36 +0530

    commit

--- first/zomato/src/index.js
+++ first/zomato/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom';
+
 import App from './App';
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+
     <App/>
-    </Router>
   </React.StrictMode>,
   
   document.getElementById('root')
