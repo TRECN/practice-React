f0a2f88241b584a0849a96bda7286c29ebef01c1
TRECN <rishabh1103@outlook.com>
2021-08-29 22:37:12 +0530

    commit

--- first/zomato-clone/src/App.js
+++ first/zomato-clone/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter as Router} from "react-router-dom"
 import Navbar from './Components/Navbar/Navbar'
 import Tabs from './Components/Tabs/Tabs'
 
 function App() {
     return (
         <div>
-            <BrowserRouter>
+            <Router>
                 <Navbar/>
                 <Tabs/>
-            </BrowserRouter>
+            </Router>
 
         </div>
     )
