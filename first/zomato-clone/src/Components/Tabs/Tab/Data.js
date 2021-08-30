9075248f014de462298d7f787a98f48aa07f5dc1
TRECN <rishabh1103@outlook.com>
2021-08-30 20:47:22 +0530

    commit

--- first/zomato-clone/src/Components/Tabs/Tab/Data.js
+++ first/zomato-clone/src/Components/Tabs/Tab/Data.js
@@ -3,7 +3,7 @@ import plate_active from '../../../assets/tabs/plate_active.webp'
 import nightlife_active from '../../../assets/tabs/Nightlife_active.webp'
 import nutrition_active from '../../../assets/tabs/Nutrition_active.webp'
 import delivery from '../../../assets/tabs/delivery.webp'
-
+import plate from '../../../assets/tabs/plate.webp'
 import nightlife from ''
 export const TabObjone={
     img:delivery_active,
