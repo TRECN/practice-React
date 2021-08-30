872baf55c6cf86d9367dc3e0906d37dddd9bd4fc
TRECN <rishabh1103@outlook.com>
2021-08-30 20:52:51 +0530

    commit

--- first/zomato-clone/src/Components/Tabs/Tabs.js
+++ first/zomato-clone/src/Components/Tabs/Tabs.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Tab from './Tab/Tab'
 import './Tabs.css'
-import {Delivery, Dining, Nightlife} from '../Tabs/Tab/Data'
+import {Delivery, Dining, Nightlife, Nutrition} from '../Tabs/Tab/Data'
 function Tabs() {
     return (
         <div className='tabs'>
             <Tab {...Delivery}/>
             <Tab {...Dining}/>
             <Tab {...Nightlife}/>
-            <Tab {...}/>
+            <Tab {...Nutrition}/>
         </div>
     )
 }
