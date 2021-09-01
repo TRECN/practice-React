3b9b46436ce01d8c7a6ae0ad47b0a3afc29cac6b
TRECN <rishabh1103@outlook.com>
2021-09-01 22:45:22 +0530

    commit

--- first/zomato-clone/src/App.js
+++ first/zomato-clone/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import { Route } from 'react-router';
+import { BrowserRouter as Router, Switch} from "react-router-dom"
 import Navbar from './Components/Navbar/Navbar'
 import  Delivery  from './Components/Delivery/Delivery'
 
