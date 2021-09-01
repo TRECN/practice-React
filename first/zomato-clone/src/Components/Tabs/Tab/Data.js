524185e1e06a6a6053a1a0da005b89651117006f
TRECN <rishabh1103@outlook.com>
2021-09-01 22:53:09 +0530

    commit

--- first/zomato-clone/src/Components/Tabs/Tab/Data.js
+++ first/zomato-clone/src/Components/Tabs/Tab/Data.js
@@ -6,35 +6,8 @@ import delivery from '../../../assets/tabs/delivery.webp'
 import plate from '../../../assets/tabs/plate.webp'
 import nightlife from '../../../assets/tabs/Nightlife.webp'
 import nutrition from '../../../assets/tabs/Nutrition.webp'
-// export const Delivery_active={
-//     img:'delivery_active',
-//     bg:true,
-//     head_color:true,
-//     underline:true,
-//     head: 'Delivery'
-// }
-// export const Dining_active={
-//     img:'plate_active',
-//     bg:true,
-//     head_color:true,
-//     underline:true,
-//     head: 'Dining\u00A0out'
-// }
-// export const Nightlife_active={
-//     img:'nightlife_active',
-//     bg:true,
-//     head_color:true,
-//     underline:true,
-//     head: 'Nightlife'
-// }
-// export const Nutrition_active={
-//     img:'nutrition_active',
-//     bg:true,
-//     head_color:true,
-//     underline:true,
-//     head:'Nutrition',
-// }
 export const Delivery={
+    add:'delivery',
     img:'delivery',
     img1:'delivery_active',
     head_C:'red',
@@ -42,18 +15,21 @@ export const Delivery={
 }
 export const Dining={
     img:'plate',
+    add:'dining',
     img1:'plate_active',
     head_C:'red',
     head: 'Dining\u00A0Out'
 }
 export const Nightlife={
     img:'nightlife',
+    add:'nightlife',
     img1:'nightlife_active',
     head_C:'red',
     head: 'Nightlife'
 }
 export const Nutrition={
     img:'nutrition',
+    add:'nutrition',
     img1:'nutrition_active',
     head_C:'red',
     head:'Nutrition',
