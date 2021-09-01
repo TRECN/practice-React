d92f0ef2fd10bbe69c487c2b7b0ff6444188569e
TRECN <rishabh1103@outlook.com>
2021-09-01 14:03:53 +0530

    commit

--- first/zomato-clone/src/Components/Tabs/Tabs.js
+++ first/zomato-clone/src/Components/Tabs/Tabs.js
@@ -3,22 +3,11 @@ import Tab from './Tab/Tab'
 import './Tabs.css'
 import {
     Delivery,
-    Delivery_active,
      Dining,
-     Dining_active,
       Nightlife,
-      Nightlife_active,
-       Nutrition,
-       Nutrition_active} from '../Tabs/Tab/Data'
+       Nutrition,} from '../Tabs/Tab/Data'
 function Tabs() {
-    const [click,setclick]=useState(false);
 
-    const handleClick=()=>{
-        if(click)
-            return Delivery_active
-        else
-            return Delivery
-    }
 
     return (
         <div className='tabs'>
