fea4d4fce47cf32bd669e120ccf6bd49d0ff5e0a
TRECN <rishabh1103@outlook.com>
2021-09-01 23:04:00 +0530

    commit

--- first/zomato-clone/src/Components/Tabs/Tab/Data.js
+++ first/zomato-clone/src/Components/Tabs/Tab/Data.js
@@ -16,6 +16,7 @@ export const Delivery={
 }
 export const Dining={
     img:'plate',
+    C_add:'http://localhost:3000/dining',
     add:'dining',
     img1:'plate_active',
     head_C:'red',
@@ -23,6 +24,7 @@ export const Dining={
 }
 export const Nightlife={
     img:'nightlife',
+    C_add:'http://localhost:3000/nightlife',
     add:'nightlife',
     img1:'nightlife_active',
     head_C:'red',
@@ -30,6 +32,7 @@ export const Nightlife={
 }
 export const Nutrition={
     img:'nutrition',
+    C_add:'http://localhost:3000/nutrition',
     add:'nutrition',
     img1:'nutrition_active',
     head_C:'red',
