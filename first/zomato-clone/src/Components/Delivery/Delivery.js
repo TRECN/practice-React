0b5ee04de9ff3bbb129ed171c867fefb93be7406
TRECN <rishabh1103@outlook.com>
2021-09-02 21:48:25 +0530

    commit

--- first/zomato-clone/src/Components/Delivery/Delivery.js
+++ first/zomato-clone/src/Components/Delivery/Delivery.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Btn from './Btn'
 import {Filters} from '../../Data'
+import './Delivery.css'
 function Delivery() {
     return (
         <div className='delivery' style={{backgroundColor:'transparent'}}>
