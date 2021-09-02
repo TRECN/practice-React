387d28bae26b77ed1fd271243591a9d092700d32
TRECN <rishabh1103@outlook.com>
2021-09-02 21:30:14 +0530

    commit

--- first/zomato-clone/src/Components/Delivery/Delivery.js
+++ first/zomato-clone/src/Components/Delivery/Delivery.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Btn from './Btn'
+import img from './img.svg'
 
 function Delivery() {
     return (
         <div className='delivery'>
             <Btn/>
-            
         </div>
     )
 }
