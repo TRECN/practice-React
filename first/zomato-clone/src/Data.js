450cb605eb19f2364e2c59033933a0f4d736246d
TRECN <rishabh1103@outlook.com>
2021-09-02 21:33:29 +0530

    commit

---
+++ first/zomato-clone/src/Data.js
@@ -0,0 +1,37 @@
+
+export const Delivery={
+    add:'delivery',
+    C_add:'http://localhost:3000/#/',
+    C_add1:'http://localhost:3000/delivery#/',
+    img:'delivery',
+    img1:'delivery_active',
+    head_C:'red',
+    head: 'Delivery'
+}
+export const Dining={
+    img:'plate',
+    C_add:'http://localhost:3000/dining',
+    C_add1:'http://localhost:3000/dining#/',
+    add:'dining',
+    img1:'plate_active',
+    head_C:'red',
+    head: 'Dining\u00A0Out'
+}
+export const Nightlife={
+    img:'nightlife',
+    C_add:'http://localhost:3000/nightlife',
+    C_add1:'http://localhost:3000/nightlife#/',
+    add:'nightlife',
+    img1:'nightlife_active',
+    head_C:'red',
+    head: 'Nightlife'
+}
+export const Nutrition={
+    img:'nutrition',
+    C_add:'http://localhost:3000/nutrition',
+    C_add1:'http://localhost:3000/nutrition#/',
+    add:'nutrition',
+    img1:'nutrition_active',
+    head_C:'red',
+    head:'Nutrition',
+}
