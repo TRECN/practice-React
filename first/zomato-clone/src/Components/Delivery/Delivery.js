5063e09be0348fca56ab098e7a4b3d8405499184
TRECN <rishabh1103@outlook.com>
2021-09-02 22:01:32 +0530

    commit

--- first/zomato-clone/src/Components/Delivery/Delivery.js
+++ first/zomato-clone/src/Components/Delivery/Delivery.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Btn from './Btn'
-import {Filters} from '../../Data'
+import {Filters,Rating,SandH,PureVeg,DeliveryTime,GreatOffers} from '../../Data'
 import './Delivery.css'
 function Delivery() {
     return (
         <div className='delivery' style={{backgroundColor:'transparent'}}>
+            <Btn {...Filters}/>
+            <Btn {...Filters}/>
+            <Btn {...Filters}/>
+            <Btn {...Filters}/>
+            <Btn {...Filters}/>
             <Btn {...Filters}/>
         </div>
     )
