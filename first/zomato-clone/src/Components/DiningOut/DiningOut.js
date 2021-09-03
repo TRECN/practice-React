e33f4a76d4aeb17a5e2bafeb70f7a92368c5fb31
TRECN <rishabh1103@outlook.com>
2021-09-03 22:10:15 +0530

    commit

--- first/zomato-clone/src/Components/DiningOut/DiningOut.js
+++ first/zomato-clone/src/Components/DiningOut/DiningOut.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Btn from '../Btn/Btn'
 
 function DiningOut() {
     return (
         <>
             <div>
-                
+                <Btn {...Filter}/>
+                <Btn />
+                <Btn />
+                <Btn />
+                <Btn />
+                <Btn />
             </div>
         </>
     )
