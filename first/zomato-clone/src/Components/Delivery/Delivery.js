3d67f589a4b3b9749c82cd6522cc3692d3065e5e
TRECN <rishabh1103@outlook.com>
2021-09-03 22:08:04 +0530

    commit

--- first/zomato-clone/src/Components/Delivery/Delivery.js
+++ first/zomato-clone/src/Components/Delivery/Delivery.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Btn from './Btn'
+import Btn from '../Btn/Btn'
 import {Filters,Rating,SandH,PureVeg,DeliveryTime,GreatOffers} from '../../Data'
 import './Delivery.css'
 function Delivery() {
