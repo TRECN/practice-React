aa065baee394de36ba4eaaffb4c9515f3355d409
TRECN <rishabh1103@outlook.com>
2021-09-03 22:06:04 +0530

    commit

--- first/zomato-clone/src/Components/DiningOut/DiningOut.js
+++ first/zomato-clone/src/Components/DiningOut/DiningOut.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 function DiningOut() {
     return (
-        <div>
-            Diningout
-        </div>
+        <>
+            <div>
+            
+            </div>
+        </>
     )
 }
 
