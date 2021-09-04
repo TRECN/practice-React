5474bce80fe7e9b0e19276068289504a2dfda6d0
TRECN <rishabh1103@outlook.com>
2021-09-04 22:27:36 +0530

    commit

--- first/zomato-clone/src/Components/Delivery/Delivery.js
+++ first/zomato-clone/src/Components/Delivery/Delivery.js
@@ -7,8 +7,6 @@ function Delivery() {
         <>
         <div className='del' style={{backgroundColor:'transparent'}}>
             <Btn {...Filters}/>
-            <img alt="Restaurant Card" src="https://b.zmtcdn.com/data/pictures/9/2700489/bdc6c1bbaed2ae53c1e3c4b303a3e5d0_o2_featured_v2.jpg" loading="lazy" class="sc-s1isp7-5 fyZwWD"></img>
-
             <Btn {...Rating}/>
             <Btn {...SandH}/>
             <Btn {...PureVeg}/>
