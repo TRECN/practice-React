b4c33040d5115eea58dd6a150ed06a6c7336f399
TRECN <rishabh1103@outlook.com>
2021-09-05 22:09:45 +0530

    commit

--- first/zomato-clone/src/Components/Cards/Card.js
+++ first/zomato-clone/src/Components/Cards/Card.js
@@ -6,12 +6,15 @@ function Card() {
     return (
         <div className='Card'>
             <img className='im'/>
+            <div className="detail">
             <div className="detail_1">
                 <h4>Domino's Pizza</h4>
                 <div className="rating">
                     <h4>3.4</h4>
                 </div>
             </div>
+            </div>
+            
         </div>
     )
 }
