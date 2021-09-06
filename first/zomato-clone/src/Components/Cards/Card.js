ae09ba28b0da01c0ad68f8bb95be1b204c5ed38f
TRECN <rishabh1103@outlook.com>
2021-09-06 22:33:09 +0530

    commit

--- first/zomato-clone/src/Components/Cards/Card.js
+++ first/zomato-clone/src/Components/Cards/Card.js
@@ -2,7 +2,9 @@ import React from 'react'
 import img from '../../assets/cards/Jashn_the_Restaurant.webp'
 import './Card.css'
 
-function Card() {
+function Card({
+    name,
+}) {
     return (
         <div className='Card'>
             <img className='im'/>
