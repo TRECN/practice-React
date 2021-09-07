4d90d7f345686c778d7f597df1cf9f90a2bef427
TRECN <rishabh1103@outlook.com>
2021-09-07 21:22:48 +0530

    commit

--- first/zomato-clone/src/Components/Cards/Card.js
+++ first/zomato-clone/src/Components/Cards/Card.js
@@ -12,7 +12,7 @@ function Card({
 }) {
     return (
         <div className='Card'>
-            <img className='im' src='../../assets/cards/Dominos_Pizza.webp'/>
+            <img className='im' src={src}/>
             <div className="detail">
                 <div className="detail_1">
                     <div className="name">
