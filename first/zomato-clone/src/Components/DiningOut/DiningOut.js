caad852689530afbe69a8e17d4de9e91c041a98e
TRECN <rishabh1103@outlook.com>
2021-09-20 21:39:45 +0530

    commit

--- first/zomato-clone/src/Components/DiningOut/DiningOut.js
+++ first/zomato-clone/src/Components/DiningOut/DiningOut.js
@@ -22,7 +22,7 @@ function DiningOut() {
                         <img className="momo" />
                         <div className="hov">
                             <h2>Tranding This Week</h2>
-                            <h3>23 places <img className='arrow'/></h3>
+                            <h3>23 places <img className='Warrow'/></h3>
                                                        
                         </div>
                     </div>
