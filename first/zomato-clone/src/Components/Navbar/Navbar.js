3268416c2060d3cc18dd56225d99af822469520a
TRECN <rishabh1103@outlook.com>
2021-09-24 21:51:08 +0530

    commit

--- first/zomato-clone/src/Components/Navbar/Navbar.js
+++ first/zomato-clone/src/Components/Navbar/Navbar.js
@@ -53,7 +53,24 @@ function Navbar() {
             </div>
 
 
-            
+            <div className={`container3 ${navbarOpen?'responsive':''}`}>
+                <ul className='links'>
+                    <li><a href="/">Log in</a></li>
+                    <li><a href="/">Sign up</a></li>
+                </ul>
+                <div className="cont">
+                    <div className="search-bar">
+                        <div className="drop">
+                            <TiLocation className='location-icon'/>
+                            <select className='dropdown'>
+                                <option>Ranchi</option>
+                            </select>
+                        </div>
+                        <BsSearch className='search-icon'/>
+                        <input type="text" placeholder='Search for restaurant, cuisine or dish' name="" className='search-text' />
+                    </div>
+                </div>
+            </div>
             
         </>
     )
