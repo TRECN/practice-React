f8906564442b6a6eb010d2f4e378916f11b84c6b
TRECN <rishabh1103@outlook.com>
2021-09-24 21:48:26 +0530

    commit

--- first/zomato-clone/src/Components/Navbar/Navbar.js
+++ first/zomato-clone/src/Components/Navbar/Navbar.js
@@ -37,8 +37,6 @@ function Navbar() {
                     <li><a href="/">Log in</a></li>
                     <li><a href="/">Sign up</a></li>
                 </ul>
-                
-                
             </div>
             
 
