25efdfd22bbcae8d34a65564d7b1f3b8b2aaccec
TRECN <rishabh1103@outlook.com>
2021-09-25 23:03:46 +0530

    commit

--- first/zomato-clone/src/Components/Cards/Card.js
+++ first/zomato-clone/src/Components/Cards/Card.js
@@ -42,13 +42,7 @@ function Card({
                         <h4>{time}</h4>
                     </div>
                 <div className="detail_3">
-                <div className="maxsafe">
-                        <img className='max'/>
-                    </div>
-                    <div className="notice">
-                        <h4>{notice}</h4>
-                    </div>   
-                </div>  
+                
             </div>
         </div>
     )
