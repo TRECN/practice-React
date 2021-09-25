453a2ee58409776f741c62e0dc433b4f5cd98ac3
TRECN <rishabh1103@outlook.com>
2021-09-25 23:01:28 +0530

    commit

--- first/zomato-clone/src/Components/Delivery/Delivery.js
+++ first/zomato-clone/src/Components/Delivery/Delivery.js
@@ -49,26 +49,7 @@ function Delivery() {
             <Card {...Jashn}/>
             <Card {...Punjabi}/>
             </div>
-            <div className="Cards">
-            <Card {...Punjabi}/>
-            <Card {...Dominos}/>
-            <Card {...Jashn}/>
-            </div>
-            <div className="Cards">
-            <Card {...Jashn}/>
-            <Card {...Punjabi}/>
-            <Card {...Dominos}/>
-            </div>
-            <div className="Cards">
-            <Card {...Punjabi}/>
-            <Card {...Jashn}/>
-            <Card {...Dominos}/>
-            </div>
-            <div className="Cards">
-            <Card {...Punjabi}/>
-            <Card {...Dominos}/>
-            <Card {...Jashn}/>
-            </div>
+            
         </div>
     </>
     )
