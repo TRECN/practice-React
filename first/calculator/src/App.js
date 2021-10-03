032ba07ae54ae21534af7f79a004871dca1a79fc
TRECN <rishabh1103@outlook.com>
2021-10-03 21:41:43 +0530

    commit

--- first/calculator/src/App.js
+++ first/calculator/src/App.js
@@ -33,6 +33,10 @@ function App() {
     return digits;
   }
 
+  const calculate =() =>{
+    setCalc(eval(calc).toString())
+  }
+
   return (
     <div className="App">
       <div className="calculator">
@@ -56,7 +60,7 @@ function App() {
         <button onClick={()=>updateCalc('0')}>0</button>
         <button onClick={()=>updateCalc('.')}>.</button>
         
-        <button >=</button>
+        <button onClick={calculate}>=</button>
         </div>
 
       </div>
