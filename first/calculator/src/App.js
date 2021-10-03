0d4bc92be001985741352867c840886035b8e6f3
TRECN <rishabh1103@outlook.com>
2021-10-03 21:05:02 +0530

    commit

--- first/calculator/src/App.js
+++ first/calculator/src/App.js
@@ -3,8 +3,10 @@ function App() {
     <div className="App">
       <div className="calculator">
         <div className="display">
-          <span>(0)</span>
+          <span>(0)</span>0
+
         </div>
+        <div className="operators"></div>
       </div>
     </div>
   );
