dfc83f4f31991adca5757fc491e8f7e4fe4235bf
TRECN <rishabh1103@outlook.com>
2021-10-03 21:34:01 +0530

    commit

--- first/calculator/src/App.js
+++ first/calculator/src/App.js
@@ -5,15 +5,19 @@ function App() {
 
   const ops=['/','*','+','-','.']
 
-  const updateCalc=()=>{
-    
+  const updateCalc=value=>{
+    setCalc(calc+value)
   }
 
   const createDigits=()=>{
     const digits=[]
     for(let i=1;i<10;i++){
       digits.push(
-        <button key={i}>{i}</button>
+        <button 
+          key={i} 
+          onClick={()=>updateCalc(i.toString())}>
+            {i}
+        </button>
       )
     }
     return digits;
@@ -24,24 +28,25 @@ function App() {
       <div className="calculator">
 
         <div className="display">
-          <span>(0)</span> 0
+          {result?<span>(0)</span> :''}{calc||"0"}
         </div>
 
         <div className="operators">
-        <button>/</button>
-        <button>*</button>
-        <button>+</button>
-        <button>-</button>
+        <button onClick={()=>updateCalc('/')}>/</button>
+        <button onClick={()=>updateCalc('*')}>*</button>
+        <button onClick={()=>updateCalc('+')}>+</button>
+        <button onClick={()=>updateCalc('-')}>-</button>
 
-        <button>DEL</button>
+        <button >DEL</button>
 
         </div>
 
         <div className="digits">
           {createDigits()}
-        <button>0</button>
-        <button>.</button>
-        <button>=</button>
+        <button onClick={()=>updateCalc('0')}>0</button>
+        <button onClick={()=>updateCalc('.')}>.</button>
+        
+        <button >=</button>
         </div>
 
       </div>
