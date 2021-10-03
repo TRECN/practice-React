009e760a96a6884ebe7416aa25c2eb4218192251
TRECN <rishabh1103@outlook.com>
2021-10-03 21:16:35 +0530

    commit

--- first/calculator/src/App.js
+++ first/calculator/src/App.js
@@ -13,10 +13,11 @@ function App() {
   return (
     <div className="App">
       <div className="calculator">
+        
         <div className="display">
           <span>(0)</span>0
-
         </div>
+
         <div className="operators">
         <button>/</button>
         <button>*</button>
@@ -26,12 +27,14 @@ function App() {
         <button>Del</button>
 
         </div>
+
         <div className="digits">
           {createDigits()}
         <button>0</button>
         <button>.</button>
         <button>=</button>
         </div>
+
       </div>
     </div>
   );
