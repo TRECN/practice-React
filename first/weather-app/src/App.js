12cc91eebd58c91cb196f3535bfde9cd75befbee
TRECN <rishabh1103@outlook.com>
2021-10-04 22:53:11 +0530

    commit

--- first/weather-app/src/App.js
+++ first/weather-app/src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
+
 import React, { useEffect, useState } from "react";
-import Weather from './components/weather';
+import Weather from './Weather';
 export default function App() {
   
   const [lat, setLat] = useState([]);
