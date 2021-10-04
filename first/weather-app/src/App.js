233f69312aa5669e6a45a492c6067f52270fc0a2
TRECN <rishabh1103@outlook.com>
2021-10-04 22:30:21 +0530

    commit

--- first/weather-app/src/App.js
+++ first/weather-app/src/App.js
@@ -1,6 +1,18 @@
-
+import React,{useEffect,useState} from "react";
 
 function App() {
+
+  const [lat,setLat] = useState([]);
+  const [long,setLong]=useState([]);
+
+  useEffect(()=>{
+    navigator.geolocation.getCurrentPosition(function(position){
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    })
+  });
+
+
   return (
     <div className="App">
       
