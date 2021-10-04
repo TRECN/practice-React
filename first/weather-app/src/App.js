e4701e28b3242e8b082bccf4e46754d345adf566
TRECN <rishabh1103@outlook.com>
2021-10-04 22:41:23 +0530

    commit

--- first/weather-app/src/App.js
+++ first/weather-app/src/App.js
@@ -4,16 +4,26 @@ function App() {
 
   const [lat,setLat] = useState([]);
   const [long,setLong]=useState([]);
+  const [data,setData]=useState([]);
 
   useEffect(()=>{
+    const fetchData=async()=>{
     navigator.geolocation.getCurrentPosition(function(position){
       setLat(position.coords.latitude);
       setLong(position.coords.longitude);
     });
-    console.log("Latitude is: ",lat);
-    console.log("Longitude is: ", long)
+    await fetch('${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}')
+        .then(res=>res.json())
+        .then(result=>{
+          setData(result)
+          console.log(result);
+        });
+      }
+      fetchData();
   },[lat,long]);
 
+
+  
 
   return (
     <div className="App">
