d9a66d424710a0634ba623906b24d91f0a066af7
TRECN <rishabh1103@outlook.com>
2021-11-08 15:44:42 +0530

    commit

--- first/avenger-api/src/App.js
+++ first/avenger-api/src/App.js
@@ -1,35 +1,36 @@
 
-import React, { useEffect, useState } from "react";
-
+import React, { useEffect } from "react";
 import Cards from "./components/Cards";
-import Axios from 'axios'
 export default function App() {
   
   
   const url = "https://api.tvmaze.com/search/shows?q=avengers";
   let data={}
-  const [da,setDa]=useState([])
-
+  const num=Math.floor(Math.random()*10);
   useEffect(() => {
     
-    
+    const fetchData = async () => {
+      try{
+        const resp=await fetch(url);
+        const json=await resp.json();
+        console.log(json);
+        data=json;
+        console.log(data)
+        console.log(data[num])
+      }
+      catch(error){
+        console.log('error',error);
+      }
+    }
     fetchData();
   }, [])
-
-  const fetchData = async () => {
-    try{
-      const resp= await Axios(url);
-      setDa(resp.data)
-    }
-    catch(error){
-      console.log('error',error);
-    }
-  }
   
   return (
     <div className="App">
-      <h1>{da}</h1>
-      
+      <h1>{data['8']}</h1>
+      {
+        console.log(data)
+      }
       <Cards{...data}/>
        
     </div>
