82c957b9dbc6fc7e8b72413fb0c64a39fb330c31
TRECN <rishabh1103@outlook.com>
2021-11-08 18:10:49 +0530

    commit

--- first/avenger-api/src/App.js
+++ first/avenger-api/src/App.js
@@ -1,26 +1,33 @@
 
 import React, { useEffect,useState } from "react";
-import Cards from "./components/Cards";
+//import Cards from "./components/Cards";
 export default function App() {
   
   
   const url = "https://api.tvmaze.com/search/shows?q=avengers";
+  let data;
   const Fetch=async()=>{
+    try{
     const response=await fetch(url);
-    
-
-  }
+    const json=await response.json();
+    console.log(json)
+    data=json
+    console.log(data)
+    }catch(error){
+      console.log(error)
+    }
+  };
 
   useEffect(()=>{
     Fetch()
-  })
+  },[])
   
 
 
   return (
     <div className="App">
       <h1>a</h1>
-      <Cards{...json[1]}/>
+      {/* <Cards{...json[1]}/> */}
        
     </div>
   );
