e0e5d01db20f4ff7da63168b1c821fceee24f3f0
TRECN <rishabh1103@outlook.com>
2021-11-08 15:33:08 +0530

    commit

---
+++ first/avenger-api/src/components/Cards.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Cards.css'
+export default function Cards(
+    data
+) {
+    console.log(data)
+    return (
+        <div className='cards'>
+            <div className="thumbnail">
+
+            </div>
+        </div>
+    )
+}
