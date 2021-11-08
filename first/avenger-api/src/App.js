5051991336f80593f10b298485e07046d487b141
TRECN <rishabh1103@outlook.com>
2021-11-08 15:54:23 +0530

    commit

--- first/avenger-api/src/App.js
+++ first/avenger-api/src/App.js
@@ -1,36 +1,46 @@
 
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import Cards from "./components/Cards";
+import Axios from 'axios'
 export default function App() {
   
   
   const url = "https://api.tvmaze.com/search/shows?q=avengers";
-  let data={}
-  const num=Math.floor(Math.random()*10);
-  useEffect(() => {
+  // let data={}
+  // const num=Math.floor(Math.random()*10);
+  // useEffect(() => {
     
-    const fetchData = async () => {
-      try{
-        const resp=await fetch(url);
-        const json=await resp.json();
-        console.log(json);
-        data=json;
-        console.log(data)
-        console.log(data[num])
-      }
-      catch(error){
-        console.log('error',error);
-      }
-    }
+  //   const fetchData = async () => {
+  //     try{
+  //       const resp=await fetch(url);
+  //       const json=await resp.json();
+  //       console.log(json);
+  //       data=json;
+  //       console.log(data)
+  //       console.log(data[num])
+  //     }
+  //     catch(error){
+  //       console.log('error',error);
+  //     }
+  //   }
+  //   fetchData();
+  // }, [])
+
+  const [data,setData]=useState([]);
+  
+  useEffect(()=>{
     fetchData();
-  }, [])
+  },[])
+
+  const fetchData=async()=>{
+    const response = await Axios(url);
+    setData(response['data'])
+  }
+
   
   return (
     <div className="App">
-      <h1>{data['8']}</h1>
-      {
-        console.log(data)
-      }
+      <h1></h1>
       <Cards{...data}/>
        
     </div>
