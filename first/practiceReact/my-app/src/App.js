ed951de4dcfb0e6a57a67988ac43cc75a85249ea
TRECN <rishabh1103@outlook.com>
2022-01-15 19:33:16 +0530

    commit

--- first/practiceReact/my-app/src/App.js
+++ first/practiceReact/my-app/src/App.js
@@ -2,13 +2,7 @@ import logo from './logo.svg';
 import React from 'react'
 import './App.css';
 
-class Item extends React.Component{
-  render(){
-    return(
-      <h1>Hello World</h1>
-    )
-  }
-}
+
 
 function App() {
   return (
