241364c104020a43b9a9f034027e9aff9fbb53d1
TRECN <rishabh1103@outlook.com>
2022-01-15 19:38:09 +0530

    commit

--- first/practiceReact/my-app/src/App.js
+++ first/practiceReact/my-app/src/App.js
@@ -10,6 +10,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Item name='rishabh'/>
+        <Item name='raj'/>
+
       </header>
     </div>
   );
