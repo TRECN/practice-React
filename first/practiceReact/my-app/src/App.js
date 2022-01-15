09f352bc95eaeb15b5c53cc0c2cd901e9bed9705
TRECN <rishabh1103@outlook.com>
2022-01-15 19:55:37 +0530

    commit

--- first/practiceReact/my-app/src/App.js
+++ first/practiceReact/my-app/src/App.js
@@ -7,7 +7,11 @@ import React from 'react';
 // import Item from './MyItem'
 
 class StarWars extends React.Component{
-  
+  render(){
+    return(
+      <h1>this is star Wars</h1>
+    )
+  }
 }
 
 function App() {
