7bb05b40a1495e9677434147efe6ae2f0e3c3e50
TRECN <rishabh1103@outlook.com>
2022-01-15 19:40:44 +0530

    commit

--- first/practiceReact/my-app/src/MyItem.js
+++ first/practiceReact/my-app/src/MyItem.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 class Item extends React.Component{
+    clickMe(){
+        console.log("I was clicked")
+    }
     render(){
+
+
       return(
-        <h1>Hello World {this.props.name}</h1>
+        <h1 onClick={}>Hello World {this.props.name}</h1>
       )
     }
   }
