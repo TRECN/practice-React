8f53c67c4729011ef3696abf4b4d7df03122a497
TRECN <rishabh1103@outlook.com>
2022-01-16 17:04:53 +0530

    commit

--- first/practiceReact/my-starwars/src/Components/StarWars.js
+++ first/practiceReact/my-starwars/src/Components/StarWars.js
@@ -44,16 +44,16 @@ class StarWars extends React.Component{
         return(
             <div>
                 {
-                        this.state.loaded&&
-                <div className="content">
-                    
-                        <img className='image'src={this.state.img} height='300px'/>
-                        <h1>{this.state.name}</h1>
-                        <p>{this.state.height}</p>
-                        <p>{this.state.homeworld}</p>
-                        <p className='wiki'><a href={this.state.wiki}>{this.state.wiki}</a></p>
-                    
-                </div>
+                    this.state.loaded&&
+                        <div className="content">
+                            
+                                <img className='image'src={this.state.img} height='300px'/>
+                                <h1>{this.state.name}</h1>
+                                <p>{this.state.height}</p>
+                                <p>{this.state.homeworld}</p>
+                                <p className='wiki'><a href={this.state.wiki}>{this.state.wiki}</a></p>
+                            
+                        </div>
                  }
                 <button type='button' className='btn' onClick={()=>this.getCharacter()}>Randomize</button>
             </div>
