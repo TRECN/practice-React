e0c29809646323911f422be798a43dc6d4703914
TRECN <rishabh1103@outlook.com>
2022-01-16 16:24:57 +0530

    commit

--- first/practiceReact/my-starwars/src/Components/StarWars.js
+++ first/practiceReact/my-starwars/src/Components/StarWars.js
@@ -5,7 +5,13 @@ class StarWars extends React.Component{
     render(){
         return(
             <div className="">
-                <img src="https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg" alt="" />
+                <img src="https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg"height='300px'/>
+                <h1>Luke Skywalker</h1>
+                <p>Height</p>
+                <p>HomeLand</p>
+                <ul>
+                    <li>films</li>
+                </ul>
             </div>
         )
     }
