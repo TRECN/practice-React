33b73e5c0453bd7f7bbeb0914349ea71e9ee4fa8
TRECN <rishabh1103@outlook.com>
2022-01-17 22:51:14 +0530

    commit

--- first/moviebrowser/src/components/Navbar.js
+++ first/moviebrowser/src/components/Navbar.js
@@ -20,12 +20,7 @@ const  Navbar=()=>{
         <li className="nav-item">
           <Link className="nav-link disabled" to="/" tabIndex="-1" aria-disabled="true">Coming soon</Link>
         </li>
-      </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
+ 
   </div>
 </nav>
       </div>
