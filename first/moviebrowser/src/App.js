a52a7b0c5cb024555b36cd27a8b8f552c2e03e03
TRECN <rishabh1103@outlook.com>
2022-01-17 22:10:24 +0530

    commit

--- first/moviebrowser/src/App.js
+++ first/moviebrowser/src/App.js
@@ -2,6 +2,12 @@ import './App.css';
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 
+const AboutView=()=>{
+  return(
+    <h2>Auout us</h2>
+  )
+}
+
 
 function App() {
   return (
