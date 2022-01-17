f37abd5a57e12967ec91c85a1b1c521fedcd77c3
TRECN <rishabh1103@outlook.com>
2022-01-17 22:19:19 +0530

    commit

--- first/moviebrowser/src/App.js
+++ first/moviebrowser/src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar'
 import Home from './components/Home'
-
-
 import AboutView from './components/AboutView'
+import { Router } from 'react-router-dom';
 
 function App() {
   return (
     <div >
       <Switch>
-        
+        <Router>
+          
+        </Router>
         <Navbar/>
         <Home/>    
         <AboutView/>   
