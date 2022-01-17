fc55d27ed94b89fcfdcfb3ff62df2405407deba6
TRECN <rishabh1103@outlook.com>
2022-01-17 22:52:06 +0530

    commit

--- first/moviebrowser/src/components/Navbar.js
+++ first/moviebrowser/src/components/Navbar.js
@@ -12,7 +12,14 @@ const  Navbar=()=>{
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-    
+          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/about">About</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link disabled" to="/" tabIndex="-1" aria-disabled="true">Coming soon</Link>
+        </li>
       </ul>
       <form className="d-flex">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
