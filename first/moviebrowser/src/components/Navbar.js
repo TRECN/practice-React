82a10b715d0ae1e117d2f136914e4db7e0fc611e
TRECN <rishabh1103@outlook.com>
2022-01-17 22:50:47 +0530

    commit

--- first/moviebrowser/src/components/Navbar.js
+++ first/moviebrowser/src/components/Navbar.js
@@ -2,7 +2,12 @@ import { Link } from "react-router-dom"
 
 const  Navbar=()=>{
     return(
- avbar-toggler-icon"></span>
+      <div>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <Link className="navbar-brand" to="/">Movies Browser</Link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
