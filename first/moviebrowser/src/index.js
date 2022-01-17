6d41df040b1f4356fae935e53c3f12188a49a779
TRECN <rishabh1103@outlook.com>
2022-01-17 22:17:26 +0530

    commit

--- first/moviebrowser/src/index.js
+++ first/moviebrowser/src/index.js
@@ -6,7 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom'
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+     <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
