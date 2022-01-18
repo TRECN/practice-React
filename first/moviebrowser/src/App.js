9a7ed492f474e8c74887ab60dafe798195eae750
TRECN <rishabh1103@outlook.com>
2022-01-18 20:23:35 +0530

    commit

--- first/moviebrowser/src/App.js
+++ first/moviebrowser/src/App.js
@@ -14,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutView />} />
+        <Route path='/search' element={<SearchView/>}/>
       </Routes>
     </div>
   );
