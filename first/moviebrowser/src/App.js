a5d45d69a9c30181b77205b61dc73625c128f953
TRECN <rishabh1103@outlook.com>
2022-01-18 20:21:57 +0530

    commit

--- first/moviebrowser/src/App.js
+++ first/moviebrowser/src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
+
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import AboutView from "./components/AboutView";
+import SearchView from './components/SearchView'
+
 import { Routes, Route } from "react-router-dom";
 
 function App() {
