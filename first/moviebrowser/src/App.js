29830f9afe94acea52b7a10b38961ee05cdcc63c
TRECN <rishabh1103@outlook.com>
2022-01-18 22:19:30 +0530

    commit

--- first/moviebrowser/src/App.js
+++ first/moviebrowser/src/App.js
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <div>
-      <Navbar />
+      <Navbar searchText={searchText} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutView />} />
