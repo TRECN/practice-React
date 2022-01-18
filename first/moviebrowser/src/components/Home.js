a2f832dcd6732bd483c61c33586942ac0275241e
TRECN <rishabh1103@outlook.com>
2022-01-18 20:15:06 +0530

    commit

--- first/moviebrowser/src/components/Home.js
+++ first/moviebrowser/src/components/Home.js
@@ -3,6 +3,11 @@ const Home=()=>{
     return(
       <>
         <Hero text="Hello World from Rishabh"/>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8 offset-lg-2 my-5"></div>
+          </div>
+        </div>
       </>
     )
   }
