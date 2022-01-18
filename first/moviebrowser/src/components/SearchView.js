26fe2565fb66bbf0d18f442848654829b7de862d
TRECN <rishabh1103@outlook.com>
2022-01-18 22:08:09 +0530

    commit

--- first/moviebrowser/src/components/SearchView.js
+++ first/moviebrowser/src/components/SearchView.js
@@ -2,7 +2,7 @@ import Hero from "./Hero";
 const SearchView = ({keyword,searchResults}) => {
     
   const title=`you are searching for ${keyword}`
-
+  console.log(searchResults, " are the search results ")
   return <>
     <Hero text={title}/>
   </>;
