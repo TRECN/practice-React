21942f55699e337e464b9c7ec43efaeebff6ae12
TRECN <rishabh1103@outlook.com>
2022-01-19 21:14:59 +0530

    commit

--- first/moviebrowser/src/components/SearchView.js
+++ first/moviebrowser/src/components/SearchView.js
@@ -1,18 +1,15 @@
 import Hero from "./Hero";
 
 const MovieCard = ({ movie }) => {
+  const posterUrl=`https://image.tmdb.org/t/p/w500${movie.poster_path}`
   return (
     <>
       <div className="card" style="width: 18rem;">
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={posterUrl} className="card-img-top" alt={movie.original_title} />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
+          <h5 className="card-title">{movie.original_title}</h5>
           <a href="#" className="btn btn-primary">
-            Go somewhere
+            show detail
           </a>
         </div>
       </div>
