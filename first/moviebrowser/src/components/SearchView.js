4522ff461d4762430547cf0ca713bf41f27c8743
TRECN <rishabh1103@outlook.com>
2022-01-19 21:19:25 +0530

    commit

--- first/moviebrowser/src/components/SearchView.js
+++ first/moviebrowser/src/components/SearchView.js
@@ -4,13 +4,15 @@ const MovieCard = ({ movie }) => {
   const posterUrl=`https://image.tmdb.org/t/p/w500${movie.poster_path}`
   return (
     <>
-      <div className="card" style="width: 18rem;">
-        <img src={posterUrl} className="card-img-top" alt={movie.original_title} />
-        <div className="card-body">
-          <h5 className="card-title">{movie.original_title}</h5>
-          <a href="#" className="btn btn-primary">
-            show detail
-          </a>
+      <div className="col-lg-4 col-md-3 col-2">
+        <div className="card" style={{width:'18em'}}>
+          <img src={posterUrl} className="card-img-top" alt={movie.original_title} />
+          <div className="card-body">
+            <h5 className="card-title">{movie.original_title}</h5>
+            <a href="#" className="btn btn-primary">
+              show detail
+            </a>
+          </div>
         </div>
       </div>
     </>
@@ -22,13 +24,17 @@ const SearchView = ({ keyword, searchResults }) => {
   console.log(searchResults, " are the search results ");
 
   const resultsHtml = searchResults.map((obj, i) => {
-    return <div key={i}>{obj.original_title}</div>;
+    return <MovieCard movie={obj} key={i}/>;
   });
 
   return (
     <>
       <Hero text={title} />
-      {resultsHtml}
+      <div className="container">
+        <div className="row">
+        {resultsHtml}
+        </div>
+      </div>
     </>
   );
 };
