1b1755eb196cf9929a6b490e42c0b3b6b8b7ecd1
TRECN <rishabh1103@outlook.com>
2022-01-19 21:40:40 +0530

    commit

--- first/moviebrowser/src/components/MovieView.js
+++ first/moviebrowser/src/components/MovieView.js
@@ -9,12 +9,17 @@ const AboutView = () => {
 
     useEffect(()=>{
         console.log('make a api request ',id)
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=cfc4a1b05a66247ddb5d7a51cd7c8280&language=en-US`)
+            .then(response=>response.json())
+            .then(data=>{
+                setMovieDetails(data)
+            })
 
     },[id])
 
   return (
     <>
-      <Hero text="Movie details" />
+      <Hero text={movieDetails.original_title} />
       
     </>
   );
