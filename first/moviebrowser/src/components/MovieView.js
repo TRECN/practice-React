795a8ef408f02a02da256c7ce3c2c5e7c3e48ab0
TRECN <rishabh1103@outlook.com>
2022-01-19 21:28:57 +0530

    commit

--- first/moviebrowser/src/components/MovieView.js
+++ first/moviebrowser/src/components/MovieView.js
@@ -1,6 +1,8 @@
 import Hero from "./Hero";
 import { useParams } from "react-router-dom";
 const AboutView = () => {
+    const {id}=useParams();
+    console.log(id)
   return (
     <>
       <Hero text="Movie details" />
