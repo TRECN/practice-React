2504deee64f9ce533b4c8f2769ebad3a630b3efb
TRECN <rishabh1103@outlook.com>
2022-01-19 21:38:47 +0530

    commit

--- first/moviebrowser/src/components/SearchView.js
+++ first/moviebrowser/src/components/SearchView.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const MovieCard = ({ movie }) => {
   const posterUrl=`https://image.tmdb.org/t/p/w500${movie.poster_path}`
-  const detailUrl=`/movie/${movie.id}`
+  const detailUrl=`/movies/${movie.id}`
   return (
     <>
       <div className="col-lg-3 col-md-3 col-2 my-4">
@@ -11,7 +11,7 @@ const MovieCard = ({ movie }) => {
           <img src={posterUrl} className="card-img-top" alt={movie.original_title} />
           <div className="card-body">
             <h5 className="card-title">{movie.original_title}</h5>
-            <Link to="movie/id" className="btn btn-primary">
+            <Link to={detailUrl} className="btn btn-primary">
               show detail
             </Link>
           </div>
