aaed0faebe8da512b37a1273dd71c254e3be68e6
TRECN <rishabh1103@outlook.com>
2022-01-19 21:32:41 +0530

    commit

--- first/moviebrowser/src/components/MovieView.js
+++ first/moviebrowser/src/components/MovieView.js
@@ -1,8 +1,12 @@
 import Hero from "./Hero";
 import { useParams } from "react-router-dom";
+import { useState, useEffect} from "react";
 const AboutView = () => {
     const {id}=useParams();
     console.log(id)
+
+    const [movieDetails,setMovieDetails]=useState({})
+
   return (
     <>
       <Hero text="Movie details" />
