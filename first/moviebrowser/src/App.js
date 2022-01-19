4afda26689ba94adff369cf8b5040391eed5db28
TRECN <rishabh1103@outlook.com>
2022-01-19 20:46:19 +0530

    commit

--- first/moviebrowser/src/App.js
+++ first/moviebrowser/src/App.js
@@ -14,7 +14,12 @@ function App() {
   const [searchText,setSearchText]=useState('')
 
   useEffect(()=>{
-    console.log(searchText)
+    console.log(searchText,"is the search text")
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=cfc4a1b05a66247ddb5d7a51cd7c8280&language=en-US&query=star%20wars&page=1&include_adult=false`)
+      .then(response =>response.json())
+      .then(data=>{
+        console.log(data)
+      })
   },[searchText])
 
   return (
