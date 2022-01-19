a40f595fb1b3f2f807acace71c6c066aa06bf5a0
TRECN <rishabh1103@outlook.com>
2022-01-19 20:57:54 +0530

    commit

--- first/moviebrowser/src/components/SearchView.js
+++ first/moviebrowser/src/components/SearchView.js
@@ -3,6 +3,11 @@ const SearchView = ({keyword,searchResults}) => {
     
   const title=`you are searching for ${keyword}`
   console.log(searchResults, " are the search results ")
+
+  const resultsHtml=searchResults.map((obj,i)=>{
+
+  })
+
   return <>
     <Hero text={title}/>
   </>;
