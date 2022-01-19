32f12a4295b0e2d10ea14bd24718583189545c56
TRECN <rishabh1103@outlook.com>
2022-01-19 22:00:34 +0530

    commit

--- first/moviebrowser/src/components/Hero.js
+++ first/moviebrowser/src/components/Hero.js
@@ -1,7 +1,9 @@
 const Hero = ({ text,backdrop }) => {
   return (
     <header className="bg-dark text-white p-5 hero-container">
-      <h1>{text}</h1>
+      <h1 className="hero-text">{text}</h1>
+      
+      <div className="hero-backdrop"></div>
       {backdrop}
     </header>
   );
