3bd61b132f2522519605138f9d5f0e883ad75c9b
TRECN <rishabh1103@outlook.com>
2022-01-19 21:08:06 +0530

    commit

--- first/moviebrowser/src/components/SearchView.js
+++ first/moviebrowser/src/components/SearchView.js
@@ -1,32 +1,39 @@
 import Hero from "./Hero";
 
-const MovieCard=({movie})=>{
-  return <>
-    <div className="card" style="width: 18rem;">
-  <img src="..." className="card-img-top" alt="...">
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-  </>
-}
+const MovieCard = ({ movie }) => {
+  return (
+    <>
+      <div className="card" style="width: 18rem;">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
 
+const SearchView = ({ keyword, searchResults }) => {
+  const title = `you are searching for ${keyword}`;
+  console.log(searchResults, " are the search results ");
 
-const SearchView = ({keyword,searchResults}) => {
-    
-  const title=`you are searching for ${keyword}`
-  console.log(searchResults, " are the search results ")
+  const resultsHtml = searchResults.map((obj, i) => {
+    return <div key={i}>{obj.original_title}</div>;
+  });
 
-  const resultsHtml=searchResults.map((obj,i)=>{
-    return <div key={i}>{obj.original_title}</div>
-  })
-
-  return <>
-    <Hero text={title}/>
-    {resultsHtml}
-  </>;
+  return (
+    <>
+      <Hero text={title} />
+      {resultsHtml}
+    </>
+  );
 };
 
 export default SearchView;
