141ea0517f6458b4e2e9430b338fdc94d397a3b3
TRECN <rishabh1103@outlook.com>
2022-01-20 21:59:09 +0530

    commit

--- first/movies-site/src/component/About.js
+++ first/movies-site/src/component/About.js
@@ -4,6 +4,22 @@ const About=()=>{
     return(
         <>
             <Hero text='About Us'/>
+            <div className="container">
+        <div className="row">
+          <div className="col-lg-8 offset-lg-2 my-5">
+            <p className="lead">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            libero dolorem sint assumenda corrupti cupiditate voluptatem nostrum
+            eveniet culpa non sunt voluptates cum corporis magnam aut
+            consequatur minus, nihil ea? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Est possimus, et voluptatum placeat illo quia eos
+            hic quas vero aut facilis mollitia rerum officiis eveniet eum iste
+            tempore error excepturi? Lorem ipsum dolor sit, amet consectetur
+            
+            </p>
+          </div>
+        </div>
+      </div>
         </>
     )
 }
