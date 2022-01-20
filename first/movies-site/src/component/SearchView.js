9086cb3532832980694ec8e2e6b4424ea2605df6
TRECN <rishabh1103@outlook.com>
2022-01-20 22:48:12 +0530

    commti

--- first/movies-site/src/component/SearchView.js
+++ first/movies-site/src/component/SearchView.js
@@ -2,16 +2,16 @@ import Hero from "./Hero"
 
 const SearchView=({searchText,searchResults})=>{
 
-     const ApiResult=searchResults.map((obj,i)=>{
+    const ApiResult=searchResults.map((obj,i)=>{
       return <>
           <h1>{obj.original_title}</h1>
-    </>
+        </>
     })
 
     return(
         <>
             <Hero text={searchText}/>
-            {/* {ApiResult} */}
+             {ApiResult} 
         </>
     )
 }
