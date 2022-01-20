c4ede311b65f3e04c38e6d991b6f8d9b32fa23ae
TRECN <rishabh1103@outlook.com>
2022-01-20 22:30:35 +0530

    commit

--- first/movies-site/src/component/SearchView.js
+++ first/movies-site/src/component/SearchView.js
@@ -1,3 +1,5 @@
+import Hero from "./Hero"
+
 const SearchView=({searchText,searchResult})=>{
 
     const ApiResult=searchResult.map((obj,i)=>{
@@ -6,10 +8,8 @@ const SearchView=({searchText,searchResult})=>{
 
     return(
         <>
-            <header className="bg-primary text-white p-5 ">
-                <h1>You are searching for {searchText}</h1>
-                {ApiResult}
-            </header>
+            <Hero text={searchText}/>
+            {ApiResult}
         </>
     )
 }
