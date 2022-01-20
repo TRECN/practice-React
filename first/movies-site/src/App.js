e1a1763337c24835004fe71be8d45405d7234866
TRECN <rishabh1103@outlook.com>
2022-01-20 22:30:32 +0530

    commit

--- first/movies-site/src/App.js
+++ first/movies-site/src/App.js
@@ -19,7 +19,7 @@ function App() {
       fetch(`https://api.themoviedb.org/3/search/movie?api_key=cfc4a1b05a66247ddb5d7a51cd7c8280&language=en-US&query=${searchText}&page=1&include_adult=false`)
         .then(response=>response.json())
         .then(data=>{
-          searchResult(data.result)
+          setSearchResult(data.results)
         })
     }
   },[searchText])
