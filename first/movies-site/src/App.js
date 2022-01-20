68ecff2ce35469f2089320aca398e28794c941e7
TRECN <rishabh1103@outlook.com>
2022-01-20 21:46:57 +0530

    commit

--- first/movies-site/src/App.js
+++ first/movies-site/src/App.js
@@ -3,14 +3,16 @@ import Navbar from './component/Navbar'
 import Home from './component/Home';
 import About from './component/About';
 
-
+import { Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Home/>
-      <About/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
     </div>
   );
 }
