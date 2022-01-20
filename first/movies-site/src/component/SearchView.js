8918878ae38918a51df989d8e7999243378cabc7
TRECN <rishabh1103@outlook.com>
2022-01-20 22:24:31 +0530

    commit

--- first/movies-site/src/component/SearchView.js
+++ first/movies-site/src/component/SearchView.js
@@ -1,13 +1,14 @@
 const SearchView=({searchText,searchResult})=>{
 
     const ApiResult=searchResult.map((obj,i)=>{
-
+        return <h1>obj.original_title</h1>
     })
 
     return(
         <>
             <header className="bg-primary text-white p-5 ">
                 <h1>You are searching for {searchText}</h1>
+                {ApiResult}
             </header>
         </>
     )
