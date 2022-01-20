fb31638d009cd0b9e6593ffa1ee95a55baa2b48a
TRECN <rishabh1103@outlook.com>
2022-01-20 21:39:50 +0530

    commit

--- first/movies-site/src/App.js
+++ first/movies-site/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './component/Navbar'
+import Home from './component/Home';
+
+
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
     </div>
   );
 }
