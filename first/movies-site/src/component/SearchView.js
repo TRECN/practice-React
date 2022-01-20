e939e0e6b44e1ed2223bbc7dca45e78583b54c63
TRECN <rishabh1103@outlook.com>
2022-01-20 22:14:04 +0530

    commit

--- first/movies-site/src/component/SearchView.js
+++ first/movies-site/src/component/SearchView.js
@@ -2,7 +2,7 @@ const SearchView=({searchText,searchResult})=>{
     return(
         <>
             <header className="bg-primary text-white p-5 ">
-                <h1>{searchText}</h1>
+                <h1>You are searching for {searchText}</h1>
             </header>
         </>
     )
