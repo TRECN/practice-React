0b9f4cec02f92aef300583aee1958720f2581413
TRECN <rishabh1103@outlook.com>
2022-01-20 22:07:42 +0530

    commit

--- first/movies-site/src/App.js
+++ first/movies-site/src/App.js
@@ -4,14 +4,25 @@ import Home from './component/Home';
 import About from './component/About';
 
 import { Routes,Route } from 'react-router-dom';
+import SearchView from './component/SearchView';
+import { useState } from 'react';
+
+
 
 function App() {
+
+  const [searchResult,setSearchResult]=useState({})
+  const [searchText,setSearchText]=useState('hi')
+
   return (
     <div className="App">
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
+        <Route path="/search" element={
+          <SearchView/>
+        }/>
       </Routes>
     </div>
   );
