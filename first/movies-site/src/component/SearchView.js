2d841c9c860045c1845674cce89ee69edcfe2faf
TRECN <rishabh1103@outlook.com>
2022-01-20 22:07:45 +0530

    commit

---
+++ first/movies-site/src/component/SearchView.js
@@ -0,0 +1,11 @@
+const SearchView=({searchText,searchResult})=>{
+    return(
+        <>
+            <header className="bg-primary text-white p-5 ">
+                <h1>{searchText}</h1>
+            </header>
+        </>
+    )
+}
+
+export default SearchView
