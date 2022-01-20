c4e174af58c4dc0b4ad6ea92df63088b424b2fda
TRECN <rishabh1103@outlook.com>
2022-01-20 22:43:40 +0530

    commit

--- first/movies-site/src/component/SearchView.js
+++ first/movies-site/src/component/SearchView.js
@@ -2,11 +2,11 @@ import Hero from "./Hero"
 
 const SearchView=({searchText,searchResuls})=>{
 
-    // const ApiResult=searchResults.map((obj,i)=>{
-    //    return <>
-    //        <h1>{obj.original_title}</h1>
-    //    </>
-    //  })
+     const ApiResult=searchResults.map((obj,i)=>{
+      return <>
+          <h1>{obj.original_title}</h1>
+    </>
+    })
 
     return(
         <>
