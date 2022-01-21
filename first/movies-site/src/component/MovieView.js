3968c7a3ca173ef567ea73a7802c593aa9d1335e
TRECN <rishabh1103@outlook.com>
2022-01-21 19:44:04 +0530

    commit

--- first/movies-site/src/component/MovieView.js
+++ first/movies-site/src/component/MovieView.js
@@ -1,5 +1,14 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom"
+
 const MovieView =()=>{
-    return<>
+    const {id}=useParams();
+
+    const [movieDetails,setMovieDetails]=useState({})
     
+
+    
+    return<>
+
     </>
 }
