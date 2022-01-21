c06cf5fa94c98e23959b885605357fa3d3c507c5
TRECN <rishabh1103@outlook.com>
2022-01-21 19:19:34 +0530

    commit

--- first/movies-site/src/component/SearchView.js
+++ first/movies-site/src/component/SearchView.js
@@ -1,32 +1,40 @@
-import Hero from "./Hero"
+import Hero from "./Hero";
 
-const MovieCard=({movie})=>{
-    return <>
-        <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+const MovieCard = ({ movie }) => {
+  return (
+    <>
+      <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" class="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
     </>
-}
+  );
+};
 
-const SearchView=({searchText,searchResults})=>{
+const SearchView = ({ searchText, searchResults }) => {
+  const ApiResult = searchResults.map((obj, i) => {
+    return (
+      <>
+        <h1>{obj.original_title}</h1>
+      </>
+    );
+  });
 
-    const ApiResult=searchResults.map((obj,i)=>{
-      return <>
-          <h1>{obj.original_title}</h1>
-        </>
-    })
+  return (
+    <>
+      <Hero text={searchText} />
+      {ApiResult}
+    </>
+  );
+};
 
-    return(
-        <>
-            <Hero text={searchText}/>
-             {ApiResult} 
-        </>
-    )
-}
-
-export default SearchView
+export default SearchView;
