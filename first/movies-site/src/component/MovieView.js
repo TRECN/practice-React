59ca75fbcbd45ab52b4d097af2ed128df2432ad5
TRECN <rishabh1103@outlook.com>
2022-01-21 19:46:18 +0530

    commit

--- first/movies-site/src/component/MovieView.js
+++ first/movies-site/src/component/MovieView.js
@@ -5,7 +5,15 @@ const MovieView =()=>{
     const {id}=useParams();
 
     const [movieDetails,setMovieDetails]=useState({})
-    
+
+
+    const getMovieApi=()=>{
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=cfc4a1b05a66247ddb5d7a51cd7c8280&language=en-US`)
+            .then(response=>response.json())
+            .then(data=>{
+                setMovieDetails(data)
+            })
+    }
 
     
     return<>
