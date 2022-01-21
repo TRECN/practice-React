d47257eea5f2914c026830c3d39ec632b050e62b
TRECN <rishabh1103@outlook.com>
2022-01-21 19:23:13 +0530

    commit

--- first/movies-site/src/component/SearchView.js
+++ first/movies-site/src/component/SearchView.js
@@ -7,7 +7,7 @@ const MovieCard = ({ movie }) => {
       <div class="card" style="width: 18rem;">
         <img src="..." class="card-img-top" alt="..." />
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
+          <h5 class="card-title">{movie.original_title}</h5>
           <p class="card-text">
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
@@ -25,15 +25,21 @@ const SearchView = ({ searchText, searchResults }) => {
   const ApiResult = searchResults.map((obj, i) => {
     return (
       <>
-        <h1>{obj.original_title}</h1>
+        <h1 key={i}>{obj.original_title}</h1>
       </>
     );
   });
 
+  const title=`You are searching for ${searchText}`
+
   return (
     <>
-      <Hero text={searchText} />
-      {ApiResult}
+      <Hero text={title} />
+      <div className="container">
+          <div className="row">
+              {ApiResult}
+          </div>
+      </div>
     </>
   );
 };
