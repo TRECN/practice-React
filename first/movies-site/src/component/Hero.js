1a06b7515a34f2da76d82c43da7e4767fd121865
TRECN <rishabh1103@outlook.com>
2022-01-21 20:06:14 +0530

    commit

--- first/movies-site/src/component/Hero.js
+++ first/movies-site/src/component/Hero.js
@@ -2,7 +2,7 @@ const Hero=({text,backdropPath})=>{
     return(
         <>
             <header className="bg-primary text-white p-5 hero-container">
-                <h1>{text}</h1>
+                <h1 className="hero-text">{text}</h1>
                 {backdropPath &&
                     <div className="hero-background" style={{backgroundImage:`url(${backdropPath})`}}></div>
                 }
