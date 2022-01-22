32167eb9770c4d8aaa284e9eeb04dbf89768b904
TRECN <rishabh1103@outlook.com>
2022-01-22 21:54:07 +0530

    commit

--- first/moviebrowser/src/components/MovieView.js
+++ first/moviebrowser/src/components/MovieView.js
@@ -24,7 +24,10 @@ const MovieView = () => {
         if(isLoading){
             return <Hero text="Loading..."/>
         }
-        
+        if(movieDetails){
+            const posterPath=`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`
+            const backdropUrl= `https://image.tmdb.org/t/p/original${movieDetails.backdrop_path}`
+            return <>
                 <Hero text={movieDetails.original_title}  backdrop={backdropUrl}/>
                 <div className="container my-4">
                     <div className="row">
