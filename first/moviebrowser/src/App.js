26c6dd01f71320c4a6c6303a6a620bddb02d077b
TRECN <rishabh1103@outlook.com>
2022-01-22 21:48:23 +0530

    commit

--- first/moviebrowser/src/App.js
+++ first/moviebrowser/src/App.js
@@ -15,7 +15,16 @@ function App() {
   const [searchResults,setSearchResults]=useState([])
   const [searchText,setSearchText]=useState('')
 
-  
+  useEffect(()=>{
+    if(searchText){
+      console.log(searchText,"is the search text")
+      fetch(`https://api.themoviedb.org/3/search/movie?api_key=cfc4a1b05a66247ddb5d7a51cd7c8280&language=en-US&query=${searchText}&page=1&include_adult=false`)
+        .then(response =>response.json())
+        .then(data=>{
+          setSearchResults(data.results)
+      })
+    }
+  },[searchText])
 
   return (
     <div>
