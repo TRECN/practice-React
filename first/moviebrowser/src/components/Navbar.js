8f7e3a2a033a41b559a4e522939a6963a1b08a02
TRECN <rishabh1103@outlook.com>
2022-01-22 21:50:59 +0530

    commit

--- first/moviebrowser/src/components/Navbar.js
+++ first/moviebrowser/src/components/Navbar.js
@@ -39,7 +39,14 @@ const Navbar = ({ searchText, setSearchText }) => {
                 </Link>
               </li>
               <li className="nav-item">
-                
+                <Link
+                  className="nav-link disabled"
+                  to="/"
+                  tabIndex="-1"
+                  aria-disabled="true"
+                >
+                  Coming soon
+                </Link>
               </li>
             </ul>
             <form className="d-flex">
