49d728fd9d271deb35445f7fae0cc2b03e6f6664
TRECN <rishabh1103@outlook.com>
2022-01-22 21:44:55 +0530

    commit

--- first/moviebrowser/src/components/Navbar.js
+++ first/moviebrowser/src/components/Navbar.js
@@ -1,7 +1,12 @@
 import {useNavigate, Link } from "react-router-dom";
 //cfc4a1b05a66247ddb5d7a51cd7c8280
 //https://api.themoviedb.org/3/search/movie?api_key=cfc4a1b05a66247ddb5d7a51cd7c8280&language=en-US&query=star%20wars&page=1&include_adult=false
-
+const Navbar = ({ searchText, setSearchText }) => {
+  const history=useNavigate()
+  const updateSearch = (e) => {
+    history('/search')
+    setSearchText(e.target.value);
+  };
 
   return (
     <div>
