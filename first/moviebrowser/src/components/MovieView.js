ef77dbf96cc0ba0b404373d73e042d001ea6481e
TRECN <rishabh1103@outlook.com>
2022-01-22 21:53:28 +0530

    commit

--- first/moviebrowser/src/components/MovieView.js
+++ first/moviebrowser/src/components/MovieView.js
@@ -34,7 +34,12 @@ const MovieView = () => {
                         <div className="col-md-3">
                             <img src={posterPath} alt="..." className="img-fluid shadow rounded"/>
                         </div>
-                        
+                        <div className="col-md-9">
+                            <h2>{movieDetails.original_title}</h2>
+                            <p className="lead">
+                                {movieDetails.overview}
+                            </p>
+                        </div>
                     </div>
                 </div>
             </>
