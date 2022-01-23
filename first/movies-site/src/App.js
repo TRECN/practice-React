02db2ce7f5420d6de331cf9ff8d9909763a55b10
TRECN <rishabh1103@outlook.com>
2022-01-23 20:58:51 +0530

    commit

--- first/movies-site/src/App.js
+++ first/movies-site/src/App.js
@@ -1,14 +1,3 @@
-import './App.css';
-import Navbar from './component/Navbar'
-import Home from './component/Home';
-import About from './component/About';
-import MovieView from './component/MovieView';
-
-import { Routes,Route } from 'react-router-dom';
-import SearchView from './component/SearchView';
-import { useEffect, useState } from 'react';
-
-
 
 function App() {
 
