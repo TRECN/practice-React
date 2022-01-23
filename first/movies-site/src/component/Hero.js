dff2498ba6a6db165c741a57ecb5573b3b47bfe1
TRECN <rishabh1103@outlook.com>
2022-01-23 21:04:56 +0530

    commit

--- first/movies-site/src/component/Hero.js
+++ first/movies-site/src/component/Hero.js
@@ -1,1 +1,14 @@
- default Hero
+const Hero=({text,backdropPath})=>{
+    return(
+        <>
+            <header className="bg-dark text-white p-5 hero-container">
+                <h1 className="hero-text">{text}</h1>
+                {backdropPath &&
+                    <div className="hero-background" style={{backgroundImage:`url(${backdropPath})`}}></div>
+                }
+            </header>
+        </>
+    )
+}
+
+export default Hero
