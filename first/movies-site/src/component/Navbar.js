d1c5f26ef7329dad17e3b10650818b379ca3feb2
TRECN <rishabh1103@outlook.com>
2022-01-23 21:05:06 +0530

    commit

--- first/movies-site/src/component/Navbar.js
+++ first/movies-site/src/component/Navbar.js
@@ -1,32 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
-const Navbar = ({searchText, setSearchText}) => {
-    const history=useNavigate()
-    const updateSearch=(e)=>{
-      history('/search')
-        setSearchText(e.target.value)
-      
-
-    }
-
-  return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
-            Navbar
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+<div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <Link className="nav-link active" aria-current="page" to="/">
