54ea19786cd5e62b10bc4f1bd470fe131b68b784
TRECN <rishabh1103@outlook.com>
2022-01-24 21:03:15 +0530

    commit

--- first/movies-site/src/component/Navbar.js
+++ first/movies-site/src/component/Navbar.js
@@ -40,7 +40,18 @@ const Navbar = ({searchText, setSearchText}) => {
               </li>
             
             </ul>
-           
+            <form className="d-flex">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                value={searchText}
+                onChange={updateSearch}
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
             </form>
           </div>
         </div>
