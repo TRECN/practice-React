d65decff9c6285ae1dd15012b1abffa4847236c8
TRECN <rishabh1103@outlook.com>
2022-01-24 20:58:11 +0530

    commit

--- first/movies-site/src/component/Home.js
+++ first/movies-site/src/component/Home.js
@@ -4,32 +4,7 @@ const Home = () => {
   return (
     <>
       <Hero text="Welcome to Movies site" />
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-8 offset-lg-2 my-5">
-            <p className="lead">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-            libero dolorem sint assumenda corrupti cupiditate voluptatem nostrum
-            eveniet culpa non sunt voluptates cum corporis magnam aut
-            consequatur minus, nihil ea? Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Est possimus, et voluptatum placeat illo quia eos
-            hic quas vero aut facilis mollitia rerum officiis eveniet eum iste
-            tempore error excepturi? Lorem ipsum dolor sit, amet consectetur
-            adipisicing elit. Officia eaque, aliquam autem alias hic possimus
-            aperiam porro iste officiis reprehenderit quia quod! Maxime aperiam
-            a, eligendi animi nemo magnam quo. Lorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Eos, amet! Mollitia temporibus,
-            distinctio doloremque consectetur architecto nostrum itaque a iste
-            enim laboriosam quod animi aspernatur officiis earum vitae ex non.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. In id
-            dolores aperiam hic rem eveniet perferendis doloremque aut
-            praesentium. Dignissimos iusto soluta et laborum unde. Eveniet est
-            similique hic dignissimos.
-            </p>
-          </div>
-        </div>
-      </div>
-    </>
+
   );
 };
 
