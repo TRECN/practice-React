12dc3b31a1d7e6e2835b016ddf3d1bd2c74981b0
TRECN <rishabh1103@outlook.com>
2022-01-24 21:02:10 +0530

    commit

--- first/movies-site/src/component/SearchView.js
+++ first/movies-site/src/component/SearchView.js
@@ -24,20 +24,7 @@ const SearchView = ({ searchText, searchResults }) => {
   const ApiResult = searchResults.map((obj, i) => {
     return   <MovieCard movie={obj} key={i}/>
       
-  });
 
-  const title=`You are searching for ${searchText}`
-
-  return (
-    <>
-      <Hero text={title} />
-      <div className="container">
-          <div className="row">
-              {ApiResult}
-          </div>
-      </div>
-    </>
-  );
 };
 
 export default SearchView;
