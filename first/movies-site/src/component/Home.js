d65ca2f0f99243d544478cd3bb3af95132302816
TRECN <rishabh1103@outlook.com>
2022-02-04 21:36:01 +0530

    commit

--- first/movies-site/src/component/Home.js
+++ first/movies-site/src/component/Home.js
@@ -8,15 +8,7 @@ const Home = () => {
         <div className="row">
           <div className="col-lg-8 offset-lg-2 my-5">
             <p className="lead">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-            libero dolorem sint assumenda corrupti cupiditate voluptatem nostrum
-            eveniet culpa non sunt voluptates cum corporis magnam aut
-            consequatur minus, nihil ea? Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Est possimus, et voluptatum placeat illo quia eos
-            hic quas vero aut facilis mollitia rerum officiis eveniet eum iste
-            tempore error excepturi? Lorem ipsum dolor sit, amet consectetur
-            adipisicing elit. Officia eaque, aliquam autem alias hic possimus
-            aperiam porro iste officiis reprehenderit quia quod! Maxime aperiam
+ officiis reprehenderit quia quod! Maxime aperiam
             a, eligendi animi nemo magnam quo. Lorem, ipsum dolor sit amet
             consectetur adipisicing elit. Eos, amet! Mollitia temporibus,
             distinctio doloremque consectetur architecto nostrum itaque a iste
