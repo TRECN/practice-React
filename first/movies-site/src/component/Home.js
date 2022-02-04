f4bd8d63a74e9e75c6c9e66d4ee8116649effafe
TRECN <rishabh1103@outlook.com>
2022-02-04 21:34:33 +0530

    commit

--- first/movies-site/src/component/Home.js
+++ first/movies-site/src/component/Home.js
@@ -1,11 +1,7 @@
 import Hero from "./Hero";
 
 const Home = () => {
-  return (
-    <>
-      <Hero text="Welcome to Movies site" />
-      <div className="container">
-        <div className="row">
+
           <div className="col-lg-8 offset-lg-2 my-5">
             <p className="lead">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
