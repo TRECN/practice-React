ddc961c1aae8aaf8d82e6ed0f17050f9f592c98b
TRECN <rishabh1103@outlook.com>
2022-02-05 20:17:57 +0530

    commit

--- first/movies-site/src/component/MovieView.js
+++ first/movies-site/src/component/MovieView.js
@@ -19,6 +19,10 @@ const MovieView = () => {
       });
   };
 
+  useEffect(() => {
+    getMovieApi();
+  }, [id]);
+
   const imgPath = `https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`;
   const backdropPath = `https://image.tmdb.org/t/p/original${movieDetails.backdrop_path}`
 
