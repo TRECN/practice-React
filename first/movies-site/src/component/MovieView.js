e8eec9cb28ee2e56c952faf1d377ca51e9142f1f
TRECN <rishabh1103@outlook.com>
2022-02-05 20:17:24 +0530

    commit

--- first/movies-site/src/component/MovieView.js
+++ first/movies-site/src/component/MovieView.js
@@ -2,11 +2,6 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Hero from "./Hero";
 
-const MovieView = () => {
-  const { id } = useParams();
-
-  const [movieDetails, setMovieDetails] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
 
   const getMovieApi = () => {
     fetch(
