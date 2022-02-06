469a38a436907f0787034ffbe0aaaf98eb75c4bd
TRECN <rishabh1103@outlook.com>
2022-02-06 22:11:13 +0530

    commit

--- first/movies-site/src/component/MovieView.js
+++ first/movies-site/src/component/MovieView.js
@@ -31,22 +31,7 @@ const MovieView = () => {
       return <Hero text="Loading..." />;
     } if(movieDetails) {
       return (
-        <>
-          <Hero text={movieDetails.original_title} backdropPath={backdropPath}/>
-          <div className="container my-5">
-            <div className="row">
-              <div className="col-md-3">
-                <img src={imgPath} alt="" className="img-fluid rounded" />
-              </div>
-              <div className="col-md-9">
-                <h2>{movieDetails.original_title}</h2>
-                <p className="lead">{movieDetails.overview}</p>
-              </div>
-            </div>
-          </div>
-        </>
-      );
-    }
+
   };
   return renderDetail()
 };
