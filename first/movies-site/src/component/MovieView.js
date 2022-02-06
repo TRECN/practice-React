bd87c2100d378766e2ce2354a0e19e7395a7a865
TRECN <rishabh1103@outlook.com>
2022-02-06 22:11:24 +0530

    commit

--- first/movies-site/src/component/MovieView.js
+++ first/movies-site/src/component/MovieView.js
@@ -1,36 +1,4 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import Hero from "./Hero";
 
-const MovieView = () => {
-  const { id } = useParams();
-
-  const [movieDetails, setMovieDetails] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
-
-  const getMovieApi = () => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=cfc4a1b05a66247ddb5d7a51cd7c8280&language=en-US`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setMovieDetails(data);
-        setIsLoading(false);
-      });
-  };
-
-  useEffect(() => {
-    getMovieApi();
-  }, [id]);
-
-  const imgPath = `https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`;
-  const backdropPath = `https://image.tmdb.org/t/p/original${movieDetails.backdrop_path}`
-
-  const renderDetail = () => {
-    if (isLoading) {
-      return <Hero text="Loading..." />;
-    } if(movieDetails) {
-      return (
         <>
           <Hero text={movieDetails.original_title} backdropPath={backdropPath}/>
           <div className="container my-5">
