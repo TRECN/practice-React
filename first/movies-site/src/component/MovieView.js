33bdd27b3e384a06361d9df12554c2a9081636c9
TRECN <rishabh1103@outlook.com>
2022-02-06 22:08:11 +0530

    commit

--- first/movies-site/src/component/MovieView.js
+++ first/movies-site/src/component/MovieView.js
@@ -19,18 +19,6 @@ const MovieView = () => {
       });
   };
 
-  useEffect(() => {
-    getMovieApi();
-  }, [id]);
-
-  const imgPath = `https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`;
-  const backdropPath = `https://image.tmdb.org/t/p/original${movieDetails.backdrop_path}`
-
-  const renderDetail = () => {
-    if (isLoading) {
-      return <Hero text="Loading..." />;
-    } if(movieDetails) {
-      return (
         <>
           <Hero text={movieDetails.original_title} backdropPath={backdropPath}/>
           <div className="container my-5">
