7dab1996d201f3a1ce365467c5dc66950d69da67
TRECN <rishabh1103@outlook.com>
2022-02-06 22:05:46 +0530

    commit

--- first/movies-site/src/component/Navbar.js
+++ first/movies-site/src/component/Navbar.js
@@ -49,9 +49,7 @@ const Navbar = ({searchText, setSearchText}) => {
                 value={searchText}
                 onChange={updateSearch}
               />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
+             
             </form>
           </div>
         </div>
