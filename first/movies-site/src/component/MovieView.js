4bb8fa418d372d3f353a2199f76a40cc0fbf3ec2
TRECN <rishabh1103@outlook.com>
2022-02-06 22:08:57 +0530

    commit

--- first/movies-site/src/component/MovieView.js
+++ first/movies-site/src/component/MovieView.js
@@ -8,16 +8,6 @@ const MovieView = () => {
   const [movieDetails, setMovieDetails] = useState({});
   const [isLoading, setIsLoading] = useState(true);
 
-  const getMovieApi = () => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=cfc4a1b05a66247ddb5d7a51cd7c8280&language=en-US`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setMovieDetails(data);
-        setIsLoading(false);
-      });
-  };
 
         <>
           <Hero text={movieDetails.original_title} backdropPath={backdropPath}/>
