41da2a44f8e57e009b95d1356fbe97f55abcf418
TRECN <rishabh1103@outlook.com>
2022-02-08 22:45:40 +0530

    commit

--- first/movies-site/src/component/Navbar.js
+++ first/movies-site/src/component/Navbar.js
@@ -1,3 +1,10 @@
+import { Link, useNavigate } from "react-router-dom";
+const Navbar = ({searchText, setSearchText}) => {
+    const history=useNavigate()
+    const updateSearch=(e)=>{
+      history('/search')
+        setSearchText(e.target.value)
+}
 
   return (
     <div>
