265d84c047771d6622b3c43b372ce02cf85aa88f
TRECN <rishabh1103@outlook.com>
2022-02-09 22:00:37 +0530

    commit

--- first/movies-site/src/component/Navbar.js
+++ first/movies-site/src/component/Navbar.js
@@ -13,14 +13,7 @@ const Navbar = ({searchText, setSearchText}) => {
           <Link className="navbar-brand" to="/">
             Navbar
           </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+        
           >
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -59,8 +52,5 @@ const Navbar = ({searchText, setSearchText}) => {
 };
 
 export default Navbar;
-
-
-
 
 
