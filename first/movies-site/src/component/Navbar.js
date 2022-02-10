178606e3489f03d2df9ddfb4e17ea78fe911a40a
TRECN <rishabh1103@outlook.com>
2022-02-10 22:37:05 +0530

    commit

--- first/movies-site/src/component/Navbar.js
+++ first/movies-site/src/component/Navbar.js
@@ -1,4 +1,13 @@
+import { Link, useNavigate } from "react-router-dom";
+const Navbar = ({searchText, setSearchText}) => {
+    const history=useNavigate()
+    const updateSearch=(e)=>{
+      history('/search')
+        setSearchText(e.target.value)
+}
 
+  return (
+    <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
