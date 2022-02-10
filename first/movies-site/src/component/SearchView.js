0b1c369cb0cb8bfa2debf5a7d63fa21b9d39cba6
TRECN <rishabh1103@outlook.com>
2022-02-10 22:37:49 +0530

    commit

--- first/movies-site/src/component/SearchView.js
+++ first/movies-site/src/component/SearchView.js
@@ -6,7 +6,17 @@ const MovieCard = ({ movie }) => {
     const path=`https://image.tmdb.org/t/p/w500${movie.poster_path}`
     const linkMovie=`/movie/${movie.id}`
   return (
-
+    <div className="col-lg-3 col-md-3 col-2 my-4">
+      <div className="card" style={{width:'18em'}}>
+        <img src={path} className="card-img-top" alt={movie.original_title} />
+        <div className="card-body">
+          <h5 className="card-title">{movie.original_title}</h5>
+          <Link to={linkMovie} className="btn btn-primary">
+            Details
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 };
 
