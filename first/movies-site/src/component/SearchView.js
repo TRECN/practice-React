d90a4c42447e2d70b9a7e1a1c2564181f53b2c38
TRECN <rishabh1103@outlook.com>
2022-02-11 22:05:22 +0530

    commit

--- first/movies-site/src/component/SearchView.js
+++ first/movies-site/src/component/SearchView.js
@@ -7,7 +7,18 @@ const MovieCard = ({ movie }) => {
     const linkMovie=`/movie/${movie.id}`
   return (
     <div className="col-lg-3 col-md-3 col-2 my-4">
-
+      <div className="card" style={{width:'18em'}}>
+        <img src={path} className="card-img-top" alt={movie.original_title} />
+        <div className="card-body">
+          <h5 className="card-title">{movie.original_title}</h5>
+          <Link to={linkMovie} className="btn btn-primary">
+            Details
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const SearchView = ({ searchText, searchResults }) => {
   const ApiResult = searchResults.map((obj, i) => {
