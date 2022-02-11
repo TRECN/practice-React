a6af1f451c31eb5cce18e134cbbc9be116a2d982
TRECN <rishabh1103@outlook.com>
2022-02-11 22:07:36 +0530

    commit

--- first/movies-site/src/component/Navbar.js
+++ first/movies-site/src/component/Navbar.js
@@ -9,7 +9,10 @@ const Navbar = ({searchText, setSearchText}) => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Navbar
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
