53ce4a215a14639bf11493d795a3e3bdde378352
TRECN <rishabh1103@outlook.com>
2022-02-11 22:04:49 +0530

    commit

--- first/movies-site/src/component/SearchView.js
+++ first/movies-site/src/component/SearchView.js
@@ -20,6 +20,11 @@ const MovieCard = ({ movie }) => {
   );
 };
 
+const SearchView = ({ searchText, searchResults }) => {
+  const ApiResult = searchResults.map((obj, i) => {
+    return   <MovieCard movie={obj} key={i}/>
+      
+  });
 
   const title=`You are searching for ${searchText}`
 
