b7d9961ffea6c759380c47bc6ad25e2c1ee752e3
TRECN <rishabh1103@outlook.com>
2022-02-11 22:06:37 +0530

    commit

--- first/movies-site/src/component/Navbar.js
+++ first/movies-site/src/component/Navbar.js
@@ -23,7 +23,14 @@ const Navbar = ({searchText, setSearchText}) => {
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
-
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">
+                  Home
+                </Link>
+              </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/about">
                   About
