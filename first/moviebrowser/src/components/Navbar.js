0d234b9226385d06031e112e36264dd4e1aa4b0b
TRECN <rishabh1103@outlook.com>
2022-02-12 21:47:04 +0530

    commit

--- first/moviebrowser/src/components/Navbar.js
+++ first/moviebrowser/src/components/Navbar.js
@@ -7,3 +7,28 @@ const Navbar = ({ searchText, setSearchText }) => {
     history('/search')
     setSearchText(e.target.value);
   };
+
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Movies Browser
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+         
+    </div>
+  );
+};
+
+export default Navbar;
