ae9595f33a145443e9583b372f49ae347eb9379e
TRECN <rishabh1103@outlook.com>
2022-03-26 20:28:37 +0530

    commit

--- first/moviebrowser/src/App.js
+++ first/moviebrowser/src/App.js
@@ -10,6 +10,21 @@ import MovieView from './components/MovieView'
 import { Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
 
+function App() {
+
+  const [searchResults,setSearchResults]=useState([])
+  const [searchText,setSearchText]=useState('')
+
+  useEffect(()=>{
+    if(searchText){
+      console.log(searchText,"is the search text")
+      fetch(`https://api.themoviedb.org/3/search/movie?api_key=cfc4a1b05a66247ddb5d7a51cd7c8280&language=en-US&query=${searchText}&page=1&include_adult=false`)
+        .then(response =>response.json())
+        .then(data=>{
+          setSearchResults(data.results)
+      })
+    }
+  },[searchText])
 
   return (
     <div>
