3e4111315506270e80effc7b587a90efa8072fe1
TRECN <rishabh1103@outlook.com>
2022-03-26 21:03:29 +0530

    commit

---
+++ first/moviebrowser/src/components/removeNthNodeEnd.cpp
@@ -0,0 +1,16 @@
+class Solution{
+    public:
+    ListNode * removeNthFromEnd(ListNode *head,int n){
+        ListNode* p1=head, *p2=head;
+        for(int i=0;i<n;i++)
+            p1=p1->next;
+        if(!p1)
+            return head->next;
+        while(p1->next){
+            p1=p1->next;
+            p2->next->next;
+        }
+        p2->next=p2->next->next;
+        return head;
+    }
+};
