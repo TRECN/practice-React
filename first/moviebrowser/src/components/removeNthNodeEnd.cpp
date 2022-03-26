c6169ba9299aa58b29bda0a8ed14d209fda5e826
TRECN <rishabh1103@outlook.com>
2022-03-26 21:05:51 +0530

    commit

--- first/moviebrowser/src/components/removeNthNodeEnd.cpp
+++ first/moviebrowser/src/components/removeNthNodeEnd.cpp
@@ -8,7 +8,7 @@ class Solution{
             return head->next;
         while(p1->next){
             p1=p1->next;
-            p2->next->next;
+            p2=p2->next;
         }
         p2->next=p2->next->next;
         return head;
