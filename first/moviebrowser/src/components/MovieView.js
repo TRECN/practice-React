6c57a8c40ae254cf277a9fef48bfa6595d8581e4
TRECN <rishabh1103@outlook.com>
2022-03-26 20:31:03 +0530

    commit

--- first/moviebrowser/src/components/MovieView.js
+++ first/moviebrowser/src/components/MovieView.js
@@ -9,6 +9,16 @@ const MovieView = () => {
     const [isLoading, setIsLoading]=useState(true)
 
 
+    useEffect(()=>{
+        console.log('make a api request ',id)
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=cfc4a1b05a66247ddb5d7a51cd7c8280&language=en-US`)
+            .then(response=>response.json())
+            .then(data=>{
+                setMovieDetails(data)
+                setIsLoading(false)
+            })
+
+    },[id])
 
     const renderMovieDetail=()=>{
         if(isLoading){
