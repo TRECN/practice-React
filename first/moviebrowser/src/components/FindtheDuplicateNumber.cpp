1a298c815af6de8d5927a6db4400b8f0f61d23f4
TRECN <rishabh1103@outlook.com>
2022-03-29 21:26:19 +0530

    commit

--- first/moviebrowser/src/components/FindtheDuplicateNumber.cpp
+++ first/moviebrowser/src/components/FindtheDuplicateNumber.cpp
@@ -2,12 +2,19 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         int result=0;
+        int count=0;
         for(auto a:nums){
-            for(auto x:nums){
-                if(a==x){
-                    result=a;
-                    break;
+            for(int i=0;i<nums.size();i++){
+                if(a==nums[i]){
+                    count++;
                 }
+            }
+            if(count>=2){
+                result=a;
+                break;
+            }
+            else{
+                count=0;  
             }
         }
         return result;
