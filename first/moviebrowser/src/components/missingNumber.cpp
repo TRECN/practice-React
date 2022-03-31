c11a721e01aacc2bf92b2a5605f6ab09f5671476
TRECN <rishabh1103@outlook.com>
2022-03-31 23:28:26 +0530

    commit

--- first/moviebrowser/src/components/missingNumber.cpp
+++ first/moviebrowser/src/components/missingNumber.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        
+        unordered_map<int,int>m;
+        int result;
+        for(auto a:nums){
+            m[a]++;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(!m[i].second){
+                result=i;
+            }
+        }
+        return result;
     }
 };
