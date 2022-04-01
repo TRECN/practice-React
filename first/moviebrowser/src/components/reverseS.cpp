13f26fa578dd4a987596ff68d68fee7a4314faba
TRECN <rishabh1103@outlook.com>
2022-04-01 23:42:08 +0530

    commit

---
+++ first/moviebrowser/src/components/reverseS.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string reverseString(string s) {
+        int i = 0, j = s.size() - 1;
+        
+        while(j--){
+            s1=s[j];
+        }
+        
+        return s;
+    }
+};
